1b0b27729a0ad27f512fabf940f94a21e5778b49
Andres Acevedo Cartagena <andresca6@gmail.com>
2018-07-10 08:24:17 -0500

    fix(Cargar archivo con varias pagina): Se valida error al momento de cargar un archivo de excel que contenga varias paginas

--- src/Controller/Transporte/Utilidad/Transporte/CargarInformacionGuiasController.php
+++ src/Controller/Transporte/Utilidad/Transporte/CargarInformacionGuiasController.php
@@ -28,18 +28,18 @@ class CargarInformacionGuiasController extends Controller
         $em = $this->getDoctrine()->getManager();
         $paginator = $this->get('knp_paginator');
         $form = $this->createFormBuilder()
-            ->add('txtCliente',TextType::class,['required' => false])
-            ->add('btnFiltrar',SubmitType::class,['label' => 'Filtrar','attr' => ['class' => 'btn btn-sm btn-default']])
-            ->add('btnEliminar',SubmitType::class,['label' => 'Eliminar','attr' => ['class' => 'btn btn-sm btn-danger', 'style' => 'float:right']])
+            ->add('txtCliente', TextType::class, ['required' => false])
+            ->add('btnFiltrar', SubmitType::class, ['label' => 'Filtrar', 'attr' => ['class' => 'btn btn-sm btn-default']])
+            ->add('btnEliminar', SubmitType::class, ['label' => 'Eliminar', 'attr' => ['class' => 'btn btn-sm btn-danger', 'style' => 'float:right']])
             ->getForm();
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            if($form->get('btnFiltrar')->isClicked()){
-                $session->set('filtroTteGuiaCargaCliente',$form->get('txtCliente')->getData());
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('btnFiltrar')->isClicked()) {
+                $session->set('filtroTteGuiaCargaCliente', $form->get('txtCliente')->getData());
             }
-            if($form->get('btnEliminar')->isClicked()){
+            if ($form->get('btnEliminar')->isClicked()) {
                 $arrSeleccionados = $request->request->get('ChkSeleccionar');
-                if(count($arrSeleccionados) > 0){
+                if (count($arrSeleccionados) > 0) {
                     $em->getRepository(TteGuiaCarga::class)->eliminar($arrSeleccionados);
                 }
                 return $this->redirect($this->generateUrl('transporte_utilidad_transporte_cargarinformacionguias_lista'));
@@ -61,20 +61,20 @@ class CargarInformacionGuiasController extends Controller
         $em = $this->getDoctrine()->getManager();
         $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
         $arConfiguracion = $em->getRepository(GenConfiguracion::class)->find(1);
-        if(!$arConfiguracion){
+        if (!$arConfiguracion) {
             Mensajes::error('Debe de registrar una configuracion general en el sistema');
             echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
         }
         $em = $this->getDoctrine()->getManager();
         $form = $this->createFormBuilder()
             ->add('attachment', FileType::class, array('attr' => ['class' => 'btn btn-sm btn-default']))
-            ->add('btnCargar', SubmitType::class, array('label' => 'Cargar','attr' => ['class' => 'btn btn-sm btn-primary']))
-            ->add('txtCliente', TextType::class,['required' => true])
+            ->add('btnCargar', SubmitType::class, array('label' => 'Cargar', 'attr' => ['class' => 'btn btn-sm btn-primary']))
+            ->add('txtCliente', TextType::class, ['required' => true])
             ->getForm();
         $form->handleRequest($request);
         if ($form->get('btnCargar')->isClicked()) {
             $ruta = $arConfiguracion->getRutaTemporal();
-            if(!$ruta){
+            if (!$ruta) {
                 Mensajes::error('Debe de ingresar una ruta temporal en la configuracion general del sistema');
                 echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
             }
@@ -83,57 +83,61 @@ class CargarInformacionGuiasController extends Controller
             $rutaArchivo = $ruta . "archivo.xls";
             $reader = \PhpOffice\PhpSpreadsheet\IOFactory::load($rutaArchivo);
             $arrCargas = [];
-            $arrErrores = [];
             $i = 0;
-            foreach ($reader->getWorksheetIterator() as $worksheet) {
-                $highestRow = $worksheet->getHighestRow(); // e.g. 10
-                for ($row = 2; $row <= $highestRow; ++$row) {
-                    $cell = $worksheet->getCellByColumnAndRow(1, $row);
-                    $arrCargas [$i]['codigoGuia'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(2, $row);
-                    $arrCargas [$i]['remitente'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(3, $row);
-                    $arrCargas [$i]['relacion'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(4, $row);
-                    $arrCargas [$i]['documento'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(5, $row);
-                    $arrCargas [$i]['nombre'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(6, $row);
-                    $arrCargas [$i]['direccion'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(7, $row);
-                    $arrCargas [$i]['telefono'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(8, $row);
-                    $arrCargas [$i]['ciudadOrigen'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(9, $row);
-                    $arrCargas [$i]['ciudadDestino'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(10, $row);
-                    $arrCargas [$i]['comentario'] = $cell->getValue();
-                    $cell = $worksheet->getCellByColumnAndRow(11, $row);
-                    $arrCargas [$i]['vrDeclarado'] = $cell->getValue();
-                    $i++;
+            if ($reader->getSheetCount() > 1) {
+                Mensajes::error('El documento debe contener solamente una hoja');
+                echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
+            } else {
+                foreach ($reader->getWorksheetIterator() as $worksheet) {
+                    $highestRow = $worksheet->getHighestRow(); // e.g. 10
+                    for ($row = 2; $row <= $highestRow; ++$row) {
+                        $cell = $worksheet->getCellByColumnAndRow(1, $row);
+                        $arrCargas [$i]['codigoGuia'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(2, $row);
+                        $arrCargas [$i]['remitente'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(3, $row);
+                        $arrCargas [$i]['relacion'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(4, $row);
+                        $arrCargas [$i]['documento'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(5, $row);
+                        $arrCargas [$i]['nombre'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(6, $row);
+                        $arrCargas [$i]['direccion'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(7, $row);
+                        $arrCargas [$i]['telefono'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(8, $row);
+                        $arrCargas [$i]['ciudadOrigen'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(9, $row);
+                        $arrCargas [$i]['ciudadDestino'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(10, $row);
+                        $arrCargas [$i]['comentario'] = $cell->getValue();
+                        $cell = $worksheet->getCellByColumnAndRow(11, $row);
+                        $arrCargas [$i]['vrDeclarado'] = $cell->getValue();
+                        $i++;
+                    }
                 }
-            }
-            if (count($arrCargas) > 0) {
-                foreach ($arrCargas as $arrCarga) {
-                    $arGuiaCarga = new TteGuiaCarga();
-                    $arGuiaCarga->setNumero($arrCarga['codigoGuia']);
-                    $arGuiaCarga->setRemitente($arrCarga['remitente']);
-                    $arGuiaCarga->setCliente($form->get('txtCliente')->getData());
-                    $arGuiaCarga->setRelacionCliente($arrCarga['relacion']);
-                    $arGuiaCarga->setDocumentoCliente($arrCarga['documento']);
-                    $arGuiaCarga->setFechaRegistro(new \DateTime('now'));
-                    $arGuiaCarga->setNombreDestinatario($arrCarga['nombre']);
-                    $arGuiaCarga->setDireccionDestinatario($arrCarga['direccion']);
-                    $arGuiaCarga->setTelefonoDestinatario($arrCarga['telefono']);
-                    $arGuiaCarga->setCodigoCiudadOrigenFk($arrCarga['ciudadOrigen']);
-                    $arGuiaCarga->setCodigoCiudadDestinoFk($arrCarga['ciudadDestino']);
-                    $arGuiaCarga->setComentario($arrCarga['comentario']);
-                    $arGuiaCarga->setVrDeclarado((float)$arrCarga['vrDeclarado']);
-                    $em->persist($arGuiaCarga);
+                if (count($arrCargas) > 0) {
+                    foreach ($arrCargas as $arrCarga) {
+                        $arGuiaCarga = new TteGuiaCarga();
+                        $arGuiaCarga->setNumero($arrCarga['codigoGuia']);
+                        $arGuiaCarga->setRemitente($arrCarga['remitente']);
+                        $arGuiaCarga->setCliente($form->get('txtCliente')->getData());
+                        $arGuiaCarga->setRelacionCliente($arrCarga['relacion']);
+                        $arGuiaCarga->setDocumentoCliente($arrCarga['documento']);
+                        $arGuiaCarga->setFechaRegistro(new \DateTime('now'));
+                        $arGuiaCarga->setNombreDestinatario($arrCarga['nombre']);
+                        $arGuiaCarga->setDireccionDestinatario($arrCarga['direccion']);
+                        $arGuiaCarga->setTelefonoDestinatario($arrCarga['telefono']);
+                        $arGuiaCarga->setCodigoCiudadOrigenFk($arrCarga['ciudadOrigen']);
+                        $arGuiaCarga->setCodigoCiudadDestinoFk($arrCarga['ciudadDestino']);
+                        $arGuiaCarga->setComentario($arrCarga['comentario']);
+                        $arGuiaCarga->setVrDeclarado((float)$arrCarga['vrDeclarado']);
+                        $em->persist($arGuiaCarga);
+                    }
                 }
+                $em->flush();
+                echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
             }
-            $em->flush();
-            echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
         }
 
         return $this->render('transporte/utilidad/transporte/cargarInformacionGuias/cargar.html.twig', [

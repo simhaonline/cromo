ca0fcac407d5219975dbaa1e5f864776a52a9288
sebas957 <baxtian957@hotmail.com>
2018-08-08 12:06:37 -0500

    Excel en el informe de produccion

--- src/Controller/Transporte/Informe/Comercial/Guia/ProduccionClienteController.php
+++ src/Controller/Transporte/Informe/Comercial/Guia/ProduccionClienteController.php
@@ -45,7 +45,7 @@ class ProduccionClienteController extends Controller
                 if ($form->get('btnExcel')->isClicked()) {
                     $fechaDesde = $form->get('fechaDesde')->getData()->format('Y-m-d');
                     $fechaHasta = $form->get('fechaHasta')->getData()->format('Y-m-d');
-                    General::get()->setExportar($em->createQuery($this->getDoctrine()->getRepository(TteGuia::class)->informeProduccionCliente($fechaDesde, $fechaHasta))->execute(), "Guias");
+                    General::get()->setExportar($em->createQuery($this->getDoctrine()->getRepository(TteGuia::class)->informeProduccionCliente($fechaDesde, $fechaHasta))->execute(), "Produccion_Cliente");
                 }
             }
         }

47454d205816d34de5b2e98f1142b69c24a095a7
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-08-09 13:51:47 -0500

    Informe produccion

---
+++ src/Controller/Transporte/Informe/Financiero/RentabilidadController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Transporte\Informe\Financiero;
+
+use App\Entity\Transporte\TteCliente;
+use App\Entity\Transporte\TteDespacho;
+use App\Entity\Transporte\TteGuia;
+use App\Entity\Transporte\TteNovedad;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+class RentabilidadController extends Controller
+{
+   /**
+    * @Route("/transporte/informe/financiero/despacho/rentabilidad", name="transporte_informe_financiero_despacho_rentabilidad")
+    */    
+    public function lista(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $paginator  = $this->get('knp_paginator');
+        $fecha = new \DateTime('now');
+        $form = $this->createFormBuilder()
+            ->add('fechaDesde', DateType::class, ['label' => 'Fecha desde: ', 'required' => false, 'widget' => 'single_text'])
+            ->add('fechaHasta', DateType::class, ['label' => 'Fecha hasta: ', 'required' => false, 'widget' => 'single_text'])
+            ->add('btnFiltrar', SubmitType::class, array('label' => 'Filtrar'))
+            ->getForm();
+        $form->handleRequest($request);
+        $arDespachos = null;
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                if ($form->get('btnFiltrar')->isClicked()) {
+                    if($form->get('fechaDesde')->getData() && $form->get('fechaHasta')->getData()) {
+                        $arDespachos = $this->getDoctrine()->getRepository(TteDespacho::class)->rentabilidad(
+                            $form->get('fechaDesde')->getData()->format('Y-m-d'),
+                            $form->get('fechaHasta')->getData()->format('Y-m-d'))->getQuery()->getResult();
+                    }
+                }
+            }
+        }
+
+        return $this->render('transporte/informe/financiero/rentabilidad.html.twig', [
+            'arDespachos' => $arDespachos,
+            'form' => $form->createView()]);
+    }
+
+}
+
--- src/Repository/Transporte/TteDespachoRepository.php
+++ src/Repository/Transporte/TteDespachoRepository.php
@@ -906,4 +906,44 @@ class TteDespachoRepository extends ServiceEntityRepository
 
     }
 
+    /**
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function rentabilidad($fechaDesde, $fechaHasta)
+    {
+        $session = new Session();
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder()->from(TteDespacho::class, 'd')
+            ->select('d.codigoDespachoPk')
+            ->addSelect('d.fechaSalida')
+            ->addSelect('d.numero')
+            ->addSelect('d.codigoOperacionFk')
+            ->addSelect('d.codigoVehiculoFk')
+            ->addSelect('d.codigoRutaFk')
+            ->addSelect('co.nombre AS ciudadOrigen')
+            ->addSelect('cd.nombre AS ciudadDestino')
+            ->addSelect('d.cantidad')
+            ->addSelect('d.unidades')
+            ->addSelect('d.pesoReal')
+            ->addSelect('d.pesoVolumen')
+            ->addSelect('d.vrFlete')
+            ->addSelect('d.vrManejo')
+            ->addSelect('d.vrDeclara')
+            ->addSelect('d.vrFletePago')
+            ->addSelect('d.vrAnticipo')
+            ->addSelect('c.nombreCorto AS conductorNombre')
+            ->addSelect('d.estadoAprobado')
+            ->addSelect('d.estadoAutorizado')
+            ->addSelect('d.estadoAnulado')
+            ->addSelect('dt.nombre AS despachoTipo')
+            ->addSelect('d.usuario')
+            ->leftJoin('d.despachoTipoRel', 'dt')
+            ->leftJoin('d.ciudadOrigenRel', 'co')
+            ->leftJoin('d.ciudadDestinoRel ', 'cd')
+            ->leftJoin('d.conductorRel', 'c')
+            ->where("d.fechaSalida >= '" . $fechaDesde . " 00:00:00' AND d.fechaSalida <='" . $fechaHasta . " 23:59:59'");
+        $queryBuilder->orderBy('d.fechaSalida', 'DESC');
+        return $queryBuilder;
+
+    }
+
 }

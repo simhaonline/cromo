36adfe4898a3e3c9d0a7166d0c733590d0296522
sebas957 <baxtian957@hotmail.com>
2018-09-18 16:24:54 -0500

    Crud automatico para despacho recogida tipo

--- src/Form/Type/Transporte/DespachoRecogidaTipoType.php
+++ src/Form/Type/Transporte/DespachoRecogidaTipoType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class despachoRecogidaTipoType extends AbstractType
+class DespachoRecogidaTipoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {

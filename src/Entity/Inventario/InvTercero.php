090a5971826724d44c9b4f47c60a4550df310b8e
Andres Acevedo <andresca6@gmail.com>
2018-09-27 11:32:23 -0500

    Se aumenta cantidad de caracteres para el campo codigo_identificacion_fk de la entidad inventario tercero

--- src/Entity/Inventario/InvTercero.php
+++ src/Entity/Inventario/InvTercero.php
@@ -24,7 +24,7 @@ class InvTercero
     private $codigoTerceroPk;
 
     /**
-     * @ORM\Column(name="codigo_identificacion_fk", type="string", length=1, nullable=true)
+     * @ORM\Column(name="codigo_identificacion_fk", type="string", length=3, nullable=true)
      */
     private $codigoIdentificacionFk;
 
@@ -483,6 +483,38 @@ class InvTercero
     /**
      * @return mixed
      */
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+
+    /**
+     * @param mixed $cliente
+     */
+    public function setCliente($cliente): void
+    {
+        $this->cliente = $cliente;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProveedor()
+    {
+        return $this->proveedor;
+    }
+
+    /**
+     * @param mixed $proveedor
+     */
+    public function setProveedor($proveedor): void
+    {
+        $this->proveedor = $proveedor;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getCodigoPrecioVentaFk()
     {
         return $this->codigoPrecioVentaFk;
@@ -639,40 +671,5 @@ class InvTercero
     {
         $this->sucursalesTerceroRel = $sucursalesTerceroRel;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getCliente()
-    {
-        return $this->cliente;
-    }
-
-    /**
-     * @param mixed $cliente
-     */
-    public function setCliente($cliente): void
-    {
-        $this->cliente = $cliente;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getProveedor()
-    {
-        return $this->proveedor;
-    }
-
-    /**
-     * @param mixed $proveedor
-     */
-    public function setProveedor($proveedor): void
-    {
-        $this->proveedor = $proveedor;
-    }
-
-
-
 }
 

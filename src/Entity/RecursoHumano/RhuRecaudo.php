be0bcc610cb7c6755368254957886d397363ac9e
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-10-03 09:45:27 -0500

    Proceso cortesia

---
+++ src/Entity/RecursoHumano/RhuRecaudo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Entity\RecursoHumano;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RecursoHumano\RhuRecaudoRepository")
+ */
+class RhuRecaudo
+{
+
+}

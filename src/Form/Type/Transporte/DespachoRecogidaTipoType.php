2d21f32f3160e736e3d310a73835b98b3a2ee30b
sebas957 <baxtian957@hotmail.com>
2018-10-05 16:38:50 -0500

    Se agregan campos nuevos al crud automatico de despacho recogida tipo

--- src/Form/Type/Transporte/DespachoRecogidaTipoType.php
+++ src/Form/Type/Transporte/DespachoRecogidaTipoType.php
@@ -3,6 +3,8 @@
 namespace App\Form\Type\Transporte;
 
 use App\Entity\Transporte\TteDespachoRecogidaTipo;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -17,9 +19,28 @@ class DespachoRecogidaTipoType extends AbstractType
     {
         $builder
             ->add('codigoDespachoRecogidaTipoPk',TextType::class,['required' => true,'label' => 'Codigo despacho recogida tipo pk:'])
+            ->add('despachosRecogidasDespachoRecogidaTipoRel',EntityType::class,[
+                'required' => false,
+                'class' => 'App\Entity\Transporte\TteDespachoRecogidaTipo',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('drt')
+                        ->orderBy('drt.nombre', 'ASC');
+                },
+                'choice_label' => 'nombre',
+                'label' => 'Despacho recogida tipo:'
+            ])
             ->add('nombre',TextType::class,['required' => true,'label' => 'Nombre:'])
             ->add('consecutivo',NumberType::class,['required' => true,'label' => 'Consecutivo:'])
             ->add('generaMonitoreo', CheckboxType::class, array('required'  => false))
+            ->add('codigoComprobanteFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaFleteFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaRetencionFuenteFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaIndustriaComercioFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaSeguridadFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaEstampillaFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaPapeleriaFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaAnticipoFk',NumberType::class,['required' => true])
+            ->add('codigoCuentaPagarFk',NumberType::class,['required' => true])
             ->add('guardar', SubmitType::class, ['label'=>'Guardar','attr' => ['class' => 'btn btn-sm btn-primary']])
             ->add('guardarnuevo', SubmitType::class, ['label'=>'Guardar y nuevo','attr' => ['class' => 'btn btn-sm btn-primary']]);
         ;

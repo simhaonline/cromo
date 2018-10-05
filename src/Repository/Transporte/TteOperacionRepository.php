970fcc45d2481373830a203236f8d9d52a81c13b
sebas957 <baxtian957@hotmail.com>
2018-10-05 14:27:37 -0500

    Se ajusta la vista de despacho recogida y se agrega filtro por operacion

--- src/Repository/Transporte/TteOperacionRepository.php
+++ src/Repository/Transporte/TteOperacionRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository\Transporte;
 
 use App\Entity\Transporte\TteOperacion;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class TteOperacionRepository extends ServiceEntityRepository
 {
@@ -20,6 +22,31 @@ class TteOperacionRepository extends ServiceEntityRepository
             ->addSelect('op.nombre AS NOMBRE');
         $query = $this->_em->createQuery($qb->getDQL());
         return $query->execute();
+    }
+
+    /**
+     * @return array
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function llenarCombo()
+    {
+        $session = new Session();
+        $array = [
+            'class' => TteOperacion::class,
+            'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('o')
+                    ->orderBy('o.nombre', 'ASC');
+            },
+            'choice_label' => 'nombre',
+            'required' => false,
+            'empty_data' => "",
+            'placeholder' => "TODOS",
+            'data' => ""
+        ];
+        if ($session->get('filtroTteOperacion')) {
+            $array['data'] = $this->getEntityManager()->getReference(TteOperacion::class, $session->get('filtroTteOperacion'));
+        }
+        return $array;
     }
 
 }

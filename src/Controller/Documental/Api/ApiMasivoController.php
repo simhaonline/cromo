09a0b3bd49c7cee31d6dcd4f21c7a9a2a0f806b9
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-10-12 09:28:43 -0500

    Cierre de mes y costos

--- src/Controller/Documental/Api/ApiMasivoController.php
+++ src/Controller/Documental/Api/ApiMasivoController.php
@@ -3,11 +3,13 @@
 namespace App\Controller\Documental\Api;
 
 use App\Entity\Documental\DocConfiguracion;
+use App\Entity\Documental\DocDirectorio;
 use App\Entity\Documental\DocMasivo;
+use App\Entity\Documental\DocMasivoTipo;
+use App\Entity\General\GenConfiguracion;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
-
 class ApiMasivoController extends FOSRestController
 {
 
@@ -61,10 +63,11 @@ class ApiMasivoController extends FOSRestController
         set_time_limit(0);
         ini_set("memory_limit", -1);
         $em = $this->getDoctrine()->getManager();
+        $directorioTemporal = $em->getRepository(GenConfiguracion::class)->parametro('rutaTemporal');
 
         $arMasivoTipo = $em->getRepository(DocMasivoTipo::class)->find($tipo);
         $arrConfiguracion = $em->getRepository(DocConfiguracion::class)->archivoMasivo();
-        $directorioBandeja = $arrConfiguracion['rutaBandeja'];
+        //$directorioBandeja = $arrConfiguracion['rutaBandeja'];
         $directorioDestino = $arrConfiguracion['rutaAlmacenamiento'] . "/masivo/";
         if(file_exists($directorioDestino)) {
             $arDirectorio = $em->getRepository(DocDirectorio::class)->findOneBy(array('tipo' => 'M', 'codigoMasivoTipoFk' => $tipo));
@@ -91,16 +94,31 @@ class ApiMasivoController extends FOSRestController
                         die('Fallo al crear directorio...' . $directorio);
                     }
                 }
-                $origen = $directorioBandeja . "/" . "";
+                $origen = $directorioTemporal .  $identificador . ".jpg";
+                $destinoConversion = $directorioTemporal .  $identificador . ".pdf";
+
+                $imagen = getimagesize($origen);    //Sacamos la información
+                $ancho = $imagen[0] / 3.77952755905511;              //Ancho
+                $alto = $imagen[1] / 3.77952755905511;
+                $pdf = new \FPDF('L','mm',array($alto,$ancho));
+                $pdf->AliasNbPages();
+                $pdf->AddPage();
+                $pdf->SetFont('Times', '', 12);
+                $pdf->image($origen,0,0);
+                $pdf->Output($destinoConversion, 'F');
+                unlink($origen);
+                $origen = $destinoConversion;
                 if(file_exists($origen)) {
-                    $extension = "";
+                    $extension = "pdf";
+                    $archivo = $identificador . ".pdf";
+                    $tamano = filesize($origen);
                     $arMasivo = new DocMasivo();
                     $arMasivo->setIdentificador($identificador);
                     $arMasivo->setMasivoTipoRel($arMasivoTipo);
-                    $arMasivo->setArchivo("");
+                    $arMasivo->setArchivo($archivo);
                     $arMasivo->setExtension($extension);
                     $arMasivo->setDirectorio($arDirectorio->getDirectorio());
-                    $arMasivo->setTamano("");
+                    $arMasivo->setTamano($tamano);
                     $archivoDestino = rand(100000, 999999) . "_" . $identificador;
                     $arMasivo->setArchivoDestino($archivoDestino . "." . $extension);
                     $destino = $directorio . $archivoDestino . "." . $extension;
@@ -118,33 +136,6 @@ class ApiMasivoController extends FOSRestController
             Mensajes::error("No existe el directorio principal " . $directorioDestino);
         }
 
-        /*$arMasivo = $this->getDoctrine()->getManager()->getRepository(DocMasivo::class)->findOneBy(array('codigoMasivoTipoFk'=>$tipo, 'identificador' => $identificador));
-        if($arMasivo) {
-            $arrConfiguracion = $this->getDoctrine()->getManager()->getRepository(DocConfiguracion::class)->archivoMasivo();
-            //$archivo = "/almacenamiento/masivo/" . $tipo . "/" . $arMasivo->getDirectorio() . "/" . $arMasivo->getArchivoDestino();
-            $archivo = $arrConfiguracion['rutaAlmacenamiento'] . "/masivo/" . $arMasivo->getCodigoMasivoTipoFk() . "/" .  $arMasivo->getDirectorio() . "/" . $arMasivo->getArchivoDestino();
-            //$archivo = "/almacenamiento/masivo/guia/1/990023629.pdf";
-
-            $type = pathinfo($archivo, PATHINFO_EXTENSION);
-            $data = file_get_contents($archivo);
-            $size = filesize($archivo); //bytes
-            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-            return [
-                'status' => true,
-                'binary' => $base64,
-                'size' => $size,
-                'type' => $type
-
-            ];
-        } else {
-            return [
-                'status' => false,
-                'binary' => "",
-                'size' => "",
-                'type' => ""
-
-            ];
-        }*/
 
         return [
             'status' => false,

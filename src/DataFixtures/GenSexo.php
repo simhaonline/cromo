0c0b495acdfb2a4f083fe12ce349968b872a1f6d
sebas957 <baxtian957@hotmail.com>
2018-10-16 13:02:53 -0500

    Se ajustan los datafixture de estado civil - sexo - tiempo -tipo cotizante

--- src/DataFixtures/GenSexo.php
+++ src/DataFixtures/GenSexo.php
@@ -10,14 +10,14 @@ class GenSexo extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $arSexo = $manager->getRepository(GenSexo::class)->find('M');
+        $arSexo = $manager->getRepository(\App\Entity\General\GenSexo::class)->find('M');
         if(!$arSexo){
             $arSexo = new \App\Entity\General\GenSexo();
             $arSexo->setCodigoSexoPk('M');
             $arSexo->setNombre('MASCULINO');
             $manager->persist($arSexo);
         }
-        $arSexo = $manager->getRepository(GenSexo::class)->find('F');
+        $arSexo = $manager->getRepository(\App\Entity\General\GenSexo::class)->find('F');
         if(!$arSexo){
             $arSexo = new \App\Entity\General\GenSexo();
             $arSexo->setCodigoSexoPk('F');
--- src/DataFixtures/RhuTipoCotizante.php
+++ src/DataFixtures/RhuTipoCotizante.php
@@ -10,126 +10,126 @@ class RhuTipoCotizante extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('1');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('1');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('1');
             $arTipoCotizante->setNombre('Dependiente');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('2');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('2');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('2');
             $arTipoCotizante->setNombre('Servicio Doméstico');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('3');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('3');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('3');
             $arTipoCotizante->setNombre('Independiente');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('4');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('4');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('4');
             $arTipoCotizante->setNombre('Madre Comunitaria');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('12');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('12');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('12');
             $arTipoCotizante->setNombre('Aprendiz SENA en etapa lectiva');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('15');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('15');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('15');
             $arTipoCotizante->setNombre('Desempleado con subsidio de caja de compensación familiar');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('16');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('16');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('16');
             $arTipoCotizante->setNombre('Independiente agremiado o asociado');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('18');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('18');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('18');
             $arTipoCotizante->setNombre('Funcionarios públicos sin tope máximo en el IBC');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('19');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('19');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('19');
             $arTipoCotizante->setNombre('Aprendices SENA en etapa productiva');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('20');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('20');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('20');
             $arTipoCotizante->setNombre('Estudiantes (Régimen especial-Ley 789/2002)');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('21');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('21');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('21');
             $arTipoCotizante->setNombre('Estudiantes de postgrado en salud (Decreto 190 de 1996)');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('22');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('22');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('22');
             $arTipoCotizante->setNombre('Profesor de establecimiento particular');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('23');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('23');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('23');
             $arTipoCotizante->setNombre('Estudiantes (Decreto 055 de 2015)');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('30');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('30');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('30');
             $arTipoCotizante->setNombre('Dependiente entidades o universidades públicas con régimen especial en Salud');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('31');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('31');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('31');
             $arTipoCotizante->setNombre('Cooperados o precooperativas de trabajo asociado');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('32');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('32');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('32');
             $arTipoCotizante->setNombre('Cotizante miembro de la carrera diplomática o consular de un país extranjero o funcionario de organismo multilateral no sometido a la legislación c');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('33');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('33');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('33');
             $arTipoCotizante->setNombre('Beneficiario del fondo de solidaridad pensional');
             $manager->persist($arTipoCotizante);
         }
-        $arTipoCotizante = $manager->getRepository(RhuTipoCotizante::class)->find('34');
+        $arTipoCotizante = $manager->getRepository(\App\Entity\RecursoHumano\RhuTipoCotizante::class)->find('34');
         if(!$arTipoCotizante){
             $arTipoCotizante = new \App\Entity\RecursoHumano\RhuTipoCotizante();
             $arTipoCotizante->setCodigoTipoCotizantePk('34');

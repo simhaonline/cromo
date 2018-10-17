f545fdffbead805214bd534037173532e7dd5351
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-10-17 10:22:22 -0500

    Asientos

--- src/Repository/Financiero/FinAsientoDetalleRepository.php
+++ src/Repository/Financiero/FinAsientoDetalleRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository\Financiero;
 
+use App\Entity\Financiero\FinAsiento;
 use App\Entity\Financiero\FinAsientoDetalle;
+use App\Utilidades\Mensajes;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -24,6 +26,33 @@ class FinAsientoDetalleRepository extends ServiceEntityRepository
             ->addSelect('ad.vrCredito')
             ->where('ad.codigoAsientoFk = ' . $codigo);
         return $queryBuilder->getQuery();
+    }
+
+    /**
+     * @param $arPedido InvPedido
+     * @param $arrDetallesSeleccionados
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function eliminar($arAsiento, $arrDetallesSeleccionados)
+    {
+        $em = $this->getEntityManager();
+        if (!$arAsiento->getEstadoAutorizado()) {
+            if (count($arrDetallesSeleccionados)) {
+                foreach ($arrDetallesSeleccionados as $codigo) {
+                    $ar = $em->getRepository(FinAsientoDetalle::class)->find($codigo);
+                    if ($ar) {
+                        $em->remove($ar);
+                    }
+                }
+                try {
+                    $em->flush();
+                } catch (\Exception $e) {
+                    Mensajes::error('No se puede eliminar, el registro porque se encuentra en uso');
+                }
+            }
+        } else {
+            Mensajes::error('No se puede eliminar, el registro se encuentra autorizado');
+        }
     }
 
 }

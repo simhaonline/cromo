1d5b4674e7453b42ae8d7789fb9a84cdd2aa4d90
alexsrc <alexcebav@gmail.com>
2018-10-24 13:51:07 -0500

    feat(Se crea vista para lista de informe log):

--- src/Controller/General/Informe/Log/LogController.php
+++ src/Controller/General/Informe/Log/LogController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\General\Informe\Log;
 
+use App\Controller\Estructura\EntityListener;
 use App\Entity\General\GenLogOld;
 use App\Repository\General\GenLogOldRepository;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,33 +25,97 @@ class LogController extends Controller {
     var $strDqlLista = "";
 
     /**
-     * @Route("/general/informe/log/lista", name="general_informe_log_ista")
+     * @Route("/general/informe/log/lista", name="general_informe_log_lista")
      */
-    public function listaAction(Request $request) {
-        set_time_limit(0);
-        ini_set("memory_limit", -1);
+    public function lista(Request $request){
         $session = new Session();
         $em = $this->getDoctrine()->getManager();
-        $paginator = $this->get('knp_paginator');
+        $paginator  = $this->get('knp_paginator');
+        $dateFecha = new \DateTime('now');
+        $strFechaDesde = $dateFecha->format('Y/m/') . "01";
+        $intUltimoDia = $strUltimoDiaMes = date("d", (mktime(0, 0, 0, $dateFecha->format('m') + 1, 1, $dateFecha->format('Y')) - 1));
+        $strFechaHasta = $dateFecha->format('Y/m/') . $intUltimoDia;
+        $dateFechaDesde = date_create($strFechaDesde);
+        $dateFechaHasta = date_create($strFechaHasta);
         $form = $this->createFormBuilder()
-//            ->add('txtCodigoDespacho', TextType::class, ['required' => false, 'data' => $session->get('filtroCodigoDespacho'), 'attr' => ['class' => 'form-control']])
-//            ->add('txtCodigoCliente', TextType::class, ['required' => false, 'data' => $session->get('filtroTteCodigoCliente'), 'attr' => ['class' => 'form-control']])
-//            ->add('txtNombreCorto', TextType::class, ['required' => false, 'data' => $session->get('filtroTteNombreCliente'), 'attr' => ['class' => 'form-control', 'readonly' => 'reandonly']])
+            ->add('txtCodigoRegistro', TextType::class, ['required' => false, 'data' => $session->get('filtroGenLogCodigoRegistro'), 'attr' => ['class' => 'form-control']])
+            ->add('dtmFechaDesde', DateType::class, ['format'=>'yyyyMMdd', 'data' => $dateFechaDesde])
+            ->add('dtmFechaHasta', DateType::class, ['format'=>'yyyyMMdd', 'data' => $dateFechaHasta])
+            ->add('SelAccion', ChoiceType::class, [
+                'label' => 'Accion', 'data' => $session->get('TxtAccion'),
+                'placeholder' => 'Seleccione una accion',
+                'choices' => [
+                    EntityListener::ACCION_NUEVO => "CREACION",
+                    EntityListener::ACCION_ACTUALIZAR => "ACTUALIZACION",
+                    EntityListener::ACCION_ELIMINAR => "ELIMINACION",
+                ],
+                'required'=>false,
+            ])
+            ->add('filtrarFecha', CheckboxType::class, ['required'=>false, 'data'=>false])
             ->add('btnFiltrar', SubmitType::class, ['label' => 'Filtrar', 'attr' => ['class' => 'btn btn-sm btn-default']])
-//            ->add('btnExcel', SubmitType::class, array('label' => 'Excel'))
             ->getForm();
         $form->handleRequest($request);
         if ($form->get('btnFiltrar')->isClicked()) {
-//            $session->set('filtroCodigoDespacho', $form->get('txtCodigoDespacho')->getData());
-//            $session->set('filtroTteCodigoCliente', $form->get('txtCodigoCliente')->getData());
-//            $session->set('filtroTteNombreCliente', $form->get('txtNombreCorto')->getData());
+            if ($form->get('txtCodigoRegistro')->getData() != '') {
+                $session->set('filtroGenLogCodigoRegistro', $form->get('txtCodigoRegistro')->getData());
+            }
+            else {
+                $session->set('filtroGenLogCodigoRegistro', null);
+            }
+
+            if ($form->get('filtrarFecha')->getData()) {
+                $session->set('filtroGenLogFechaDesde',$form->get('dtmFechaDesde')->getData()->format('Y-m-d'));
+                $session->set('filtroGenLogFechaHasta',date_modify($form->get('dtmFechaHasta')->getData(),'+1days')->format('Y-m-d'));
+            }
+            else{
+                $session->set('filtroGenLogFechaDesde',null);
+                $session->set('filtroGenLogFechaHasta',null);
+            }
+
+            if($form->get('SelAccion')->getData()!==""){
+                $session->set('filtroGenLogAccion',$form->get('SelAccion')->getData());
+            }
+            else{
+                $session->set('filtroGenLogAccion',null);
+            }
+
         }
-        $query = $em->createQuery($em->getRepository(GenLogOld::class)->lista());
-        $arLogs = $paginator->paginate($query, $request->query->get('page', 1), 50);
-        return $this->render('general/log/lista.html.twig', [
-            'arLogs' => $arLogs,
-            'form' => $form->createView()]);
+        $arGenLog= $paginator->paginate($em->getRepository('App:General\GenLog')->lista(),$request->query->getInt('page',1),20);
+        return $this->render('general/log/lista.html.twig',
+            ['arGenLog' => $arGenLog,
+                'form' => $form->createView()]);
     }
+//QVMBXCvFHaltPm/ebXycQ7rOvgbgLFfQQvSxZVa/I/FvtsO9Ps9k9B84YP6rC+1aEio86UKY0ZxwsAEV/se7+Q==
+
+
+
+
+
+//    public function listaAction(Request $request) {
+//        set_time_limit(0);
+//        ini_set("memory_limit", -1);
+//        $session = new Session();
+//        $em = $this->getDoctrine()->getManager();
+//        $paginator = $this->get('knp_paginator');
+//        $form = $this->createFormBuilder()
+////            ->add('txtCodigoDespacho', TextType::class, ['required' => false, 'data' => $session->get('filtroCodigoDespacho'), 'attr' => ['class' => 'form-control']])
+////            ->add('txtCodigoCliente', TextType::class, ['required' => false, 'data' => $session->get('filtroTteCodigoCliente'), 'attr' => ['class' => 'form-control']])
+////            ->add('txtNombreCorto', TextType::class, ['required' => false, 'data' => $session->get('filtroTteNombreCliente'), 'attr' => ['class' => 'form-control', 'readonly' => 'reandonly']])
+//            ->add('btnFiltrar', SubmitType::class, ['label' => 'Filtrar', 'attr' => ['class' => 'btn btn-sm btn-default']])
+////            ->add('btnExcel', SubmitType::class, array('label' => 'Excel'))
+//            ->getForm();
+//        $form->handleRequest($request);
+//        if ($form->get('btnFiltrar')->isClicked()) {
+////            $session->set('filtroCodigoDespacho', $form->get('txtCodigoDespacho')->getData());
+////            $session->set('filtroTteCodigoCliente', $form->get('txtCodigoCliente')->getData());
+////            $session->set('filtroTteNombreCliente', $form->get('txtNombreCorto')->getData());
+//        }
+//        $query = $em->createQuery($em->getRepository(GenLogOld::class)->lista());
+//        $arLogs = $paginator->paginate($query, $request->query->get('page', 1), 50);
+//        return $this->render('general/log/lista.html.twig', [
+//            'arLogs' => $arLogs,
+//            'form' => $form->createView()]);
+//    }
 
 //    /**
 //     * @Route("/gen/consulta/log/lista", name="brs_gen_consulta_log_lista")
--- src/Repository/General/GenLogRepository.php
+++ src/Repository/General/GenLogRepository.php
@@ -6,6 +6,7 @@ use App\Entity\General\GenLog;
 use App\Entity\General\GenSexo;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class GenLogRepository extends ServiceEntityRepository
 {
@@ -14,51 +15,78 @@ class GenLogRepository extends ServiceEntityRepository
         parent::__construct($registry, GenSexo::class);
     }
 
-    public function listaDql($codigo = null, $usuario = null, $accion = null, $modulo = null, $fecha = "", $hoy = false, $fechaHasta = "", $entidad = "")
-    {
-        $em = $this->getEntityManager();
-        $qb = $em->createQueryBuilder();
-        $qb->from("App:General\GenLog", "le")
-            ->select("le");
-        if(!empty($codigo)) {
-            $qb->andWhere("le.codigoRegistroPk = '{$codigo}'");
+    public function lista(){
+        $session= new Session();
+        $queryBuilder=$this->getEntityManager()->createQueryBuilder()->from('App:General\GenLog','gl')
+            ->select('gl.codigoLogPk')
+            ->addSelect('gl.codigoRegistroPk')
+            ->addSelect('gl.codigoUsuarioFk')
+            ->addSelect('gl.nombreUsuario')
+            ->addSelect('gl.accion')
+            ->addSelect('gl.ruta')
+            ->addSelect('gl.fecha')
+//            ->where('gl.codigoUsuarioFk IS NOT NULL')
+            ->orderBy('gl.fecha','DESC');
+
+        if ($session->get('filtroGenLogCodigoRegistro') != '') {
+            $queryBuilder->andWhere("gl.codigoRegistroPk LIKE '%{$session->get('filtroGenLogCodigoRegistro')}%' ");
         }
-        if(!empty($usuario)) {
-            $qb->andWhere("le.codigoUsuarioFk = '{$usuario}'");
+        if($session->get('filtroGenLogFechaDesde')||$session->get('filtroGenLogFechaHasta')){
+            $queryBuilder->andWhere("gl.fecha >='{$session->get('filtroGenLogFechaDesde')}'")
+            ->andWhere("gl.fecha <='{$session->get('filtroGenLogFechaHasta')}'");
         }
-        if(!empty($accion)) {
-            $qb->andWhere("le.accion LIKE '%{$accion}%'");
+        if($session->get('filtroGenLogAccion')){
+            $queryBuilder->andWhere("gl.accion='{$session->get('filtroGenLogAccion')}'");
         }
-        if(!empty($modulo)) {
-            $qb->andWhere("le.modulo LIKE '%{$modulo}%'");
-        }
-        if($hoy){
-            $hoy = new \DateTime('now');
-            $qb->andWhere("le.fecha LIKE '%{$hoy->format('Y-m-d')}%'");
-        }
-        if($fecha != ""){
-            $qb->andWhere("le.fecha >= '{$fecha} 00:00:00'");
-        }
-        if($fechaHasta != ""){
-            $qb->andWhere("le.fecha <= '{$fechaHasta} 23:59:59'");
-        }
-        if($entidad != ""){
-            $qb->andWhere("le.nombreEntidad LIKE '%{$entidad}%'");
-        }
-        $qb->orderBy("le.fecha", "desc");
-        return $qb->getQuery()->getDQL();
+
+        return $queryBuilder;
     }
 
-    /**
-     * @param $arLog GenLog
-     */
-    public function listaCambios()
-    {
-        $em = $this->getEntityManager();
-        $qb = $em->createQueryBuilder();
-        $qb->from("App:General\GenLog", "le")
-            ->select("le");
-        $qb->orderBy("le.fecha", "asc");
-        return $qb->getQuery()->getDQL();
-    }
+//    public function listaDql($codigo = null, $usuario = null, $accion = null, $modulo = null, $fecha = "", $hoy = false, $fechaHasta = "", $entidad = "")
+//    {
+//        $em = $this->getEntityManager();
+//        $qb = $em->createQueryBuilder();
+//        $qb->from("App:General\GenLog", "le")
+//            ->select("le");
+//        if(!empty($codigo)) {
+//            $qb->andWhere("le.codigoRegistroPk = '{$codigo}'");
+//        }
+//        if(!empty($usuario)) {
+//            $qb->andWhere("le.codigoUsuarioFk = '{$usuario}'");
+//        }
+//        if(!empty($accion)) {
+//            $qb->andWhere("le.accion LIKE '%{$accion}%'");
+//        }
+//        if(!empty($modulo)) {
+//            $qb->andWhere("le.modulo LIKE '%{$modulo}%'");
+//        }
+//        if($hoy){
+//            $hoy = new \DateTime('now');
+//            $qb->andWhere("le.fecha LIKE '%{$hoy->format('Y-m-d')}%'");
+//        }
+//        if($fecha != ""){
+//            $qb->andWhere("le.fecha >= '{$fecha} 00:00:00'");
+//        }
+//        if($fechaHasta != ""){
+//            $qb->andWhere("le.fecha <= '{$fechaHasta} 23:59:59'");
+//        }
+//        if($entidad != ""){
+//            $qb->andWhere("le.nombreEntidad LIKE '%{$entidad}%'");
+//        }
+//        $qb->orderBy("le.fecha", "desc");
+//        return $qb->getQuery()->getDQL();
+//    }
+//
+//    /**
+//     * @param $arLog GenLog
+//     */
+//    public function listaCambios()
+//    {
+//        $em = $this->getEntityManager();
+//        $qb = $em->createQueryBuilder();
+//        $qb->from("App:General\GenLog", "le")
+//            ->select("le");
+//        $qb->orderBy("le.fecha", "asc");
+//        return $qb->getQuery()->getDQL();
+//    }
 }

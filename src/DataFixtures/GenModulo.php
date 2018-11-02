eb9924c1c3bfe0901cafa872d54024972df74b2c
alexsrc <alexcebav@gmail.com>
2018-11-02 10:01:27 -0500

    feat(Fixture GenModulo):

---
+++ src/DataFixtures/GenModulo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class GenModulo extends Fixture
+{
+
+
+    public function load(ObjectManager $manager)
+    {
+        $arrayGenModulo=array('Cartera','Compra','Documental','Financiero','General','Inventario','Recurso Humano','Seguridad','Transporte');
+        foreach ($arrayGenModulo as $arrGenModulo){
+        $arGenModulo = $manager->getRepository('App:General\GenModulo')->find($arrGenModulo);
+        if(!$arGenModulo) {
+            $arGenModulo = new \App\Entity\General\GenModulo();
+        $arGenModulo->setCodigoModuloPk($arrGenModulo);
+            $manager->persist($arGenModulo);
+            $manager->flush();
+        }
+        }
+    }
+}

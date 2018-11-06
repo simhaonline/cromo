5b96235b1dfa87753df8b6b9fed1f78a2c2d4b3e
Andres Acevedo <andresca6@gmail.com>
2018-11-06 11:21:05 -0500

    Modificaciones generales en recurso humano

---
+++ recursos/script/Insertar conceptos.sql
@@ -0,0 +1,11 @@
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HOL','HORAS ORDINARIAS LABORADAS',100,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HEFS','HORAS EXTRAS FESTIVAS DIURNAS',200,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HEFN','HORAS EXTRAS FESTIVAS NOCTURNAS',250,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HEOD','HORAS EXTRAS ORDINARIAS DIURNAS',125,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HEON','HORAS EXTRAS ORDINARIAS NOCTURNAS',175,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HODA','HORAS ORDINARIAS DIURNAS ADICION',0,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HFD','HORAS FESTIVAS DIURNAS',175,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HFN','HORAS FESTIVAS NOCTURNAS',210,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HD','HORA DIURNA',100,1,1,1,0);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('HN','HORA NOCTURNA',135,1,1,1,1);
+INSERT INTO rhu_concepto (codigo_concepto_pk,nombre,porcentaje,operacion,genera_ingreso_base_cotizacion,genera_ingreso_base_prestacion,recargo_nocturno) VALUES ('RN','RECARGO NOCTURNO',35,1,1,1,1);

bf805ac4f69b181c5560abfc4762e067ecdc9a59
sebas957 <baxtian957@hotmail.com>
2018-11-16 16:41:56 -0500

    Se organizan todos los form de administracion del modulo de inventario acorde al cambio realizado por el equipo de desarrollo

--- src/Form/Type/Inventario/RemisionTipoType.php
+++ src/Form/Type/Inventario/RemisionTipoType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type\Inventario;
 
 use App\Entity\Inventario\InvRemisionTipo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,10 +15,10 @@ class RemisionTipoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('codigoRemisionTipoPk',TextType::class,['label' => 'Código pedido tipo: '])
+            ->add('codigoRemisionTipoPk',TextType::class,['label' => 'Código remision tipo: '])
             ->add('nombre',TextType::class,['label' => 'Nombre: '])
+            ->add('consecutivo',NumberType::class,['label' => 'Consecutivo:'])
             ->add('guardar',SubmitType::class,['label' => 'Guardar','attr' => ['class' => 'btn btn-sm btn-primary']])
-            ->add('guardarnuevo', SubmitType::class,['label' => 'Guardar y nuevo','attr' => ['class' => 'btn btn-sm btn-primary']])
         ;
     }
 
@@ -26,5 +27,26 @@ class RemisionTipoType extends AbstractType
         $resolver->setDefaults([
             'data_class' => InvRemisionTipo::class,
         ]);
+    }
+
+    public function getEstructuraPropiedadesLista()
+    {
+        $campos = '[
+            {"campo":"codigoRemisionTipoPk",          "tipo":"pk"     ,"ayuda":"Codigo del registro",          "titulo":"ID"},
+            {"campo":"nombre",                     "tipo":"texto"  ,"ayuda":"Nombre de las remisiones tipo",       "titulo":"NOMBRE"},   
+            {"campo":"consecutivo",                "tipo":"texto"  ,"ayuda":"Consecutivo del registro",     "titulo":"NOMBRE"}             
+                                                                          
+        ]';
+        return $campos;
+    }
+
+    public function getEstructuraPropiedadesExportar()
+    {
+        $campos = '[
+            {"campo":"codigoRemisionTipoPk",          "tipo":"pk"     ,"ayuda":"Codigo del registro",          "titulo":"ID"},
+            {"campo":"nombre",                     "tipo":"texto"  ,"ayuda":"Nombre de las remisiones tipo",       "titulo":"NOMBRE"},   
+            {"campo":"consecutivo",                "tipo":"texto"  ,"ayuda":"Consecutivo del registro",     "titulo":"NOMBRE"}                                      
+        ]';
+        return $campos;
     }
 }

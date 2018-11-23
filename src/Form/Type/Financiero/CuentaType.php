7409e74dab7512c58d38fb62b1236e3aae2c2e24
sebas957 <baxtian957@hotmail.com>
2018-11-23 10:49:23 -0500

    Ajuste para el administrador de las cuentas de financiero

--- src/Form/Type/Financiero/CuentaType.php
+++ src/Form/Type/Financiero/CuentaType.php
@@ -17,6 +17,7 @@ class CuentaType extends AbstractType
         $builder
             ->add('codigoCuentaPk', TextType::class, ['label' => 'Codigo cuenta:'])
             ->add('nombre', TextType::class, ['label' => 'Nombre:'])
+            ->add('codigoCuentaPadreFk', TextType::class, ['label' => 'Codigo cuenta padre:'])
             ->add('exigeTercero', CheckboxType::class, ['label' => 'Exige tercero:', 'required' => false])
             ->add('exigeCentroCosto', CheckboxType::class, ['label' => 'Exige centro de costo', 'required' => false])
             ->add('exigeBase', CheckboxType::class, ['label' => 'Exige base', 'required' => false])
@@ -30,5 +31,34 @@ class CuentaType extends AbstractType
         $resolver->setDefaults([
             'data_class' => FinCuenta::class,
         ]);
+    }
+
+    public function getEstructuraPropiedadesLista()
+    {
+        $campos = '[
+            {"campo":"codigoCuentaPk",          "tipo":"pk"     ,"ayuda":"Codigo del registro",          "titulo":"ID"},
+            {"campo":"nombre",                  "tipo":"texto"  ,"ayuda":"Nombre de la cuenta",       "titulo":"NOMBRE"},
+            {"campo":"codigoCuentaPadreFk",     "tipo":"texto"  ,"ayuda":"Cuenta padre",       "titulo":"CUENTA PADRE"},   
+            {"campo":"exigeTercero",            "tipo":"bool"  ,"ayuda":"La cuenta exige tercero",     "titulo":"EXIGE TERCERO"},
+            {"campo":"exigeCentroCosto",        "tipo":"bool"  ,"ayuda":"La cuenta exige centro de costos",     "titulo":"EXIGE CENTRO COSTO"},
+            {"campo":"exigeBase",               "tipo":"bool"  ,"ayuda":"Exige base",     "titulo":"EXIGE BASE DE RETENCION"},
+            {"campo":"permiteMovimiento",       "tipo":"bool"  ,"ayuda":"La cuenta permite movimientos",     "titulo":"PERMITE MOVIMIENTO"}            
+                                                                          
+        ]';
+        return $campos;
+    }
+
+    public function getEstructuraPropiedadesExportar()
+    {
+        $campos = '[
+            {"campo":"codigoCuentaPk",          "tipo":"pk"     ,"ayuda":"Codigo del registro",          "titulo":"ID"},
+            {"campo":"nombre",                  "tipo":"texto"  ,"ayuda":"Nombre de la cuenta",       "titulo":"NOMBRE"},
+            {"campo":"codigoCuentaPadreFk",     "tipo":"texto"  ,"ayuda":"Cuenta padre",       "titulo":"CUENTA PADRE"},   
+            {"campo":"exigeTercero",            "tipo":"bool"  ,"ayuda":"La cuenta exige tercero",     "titulo":"EXIGE TERCERO"},
+            {"campo":"exigeCentroCosto",        "tipo":"bool"  ,"ayuda":"La cuenta exige centro de costos",     "titulo":"EXIGE CENTRO COSTO"},
+            {"campo":"exigeBase",               "tipo":"bool"  ,"ayuda":"Exige base",     "titulo":"EXIGE BASE DE RETENCION"},
+            {"campo":"permiteMovimiento",       "tipo":"bool"  ,"ayuda":"La cuenta permite movimientos",     "titulo":"PERMITE MOVIMIENTO"}                                    
+        ]';
+        return $campos;
     }
 }

428eaa0d7a77b583ac1c03621b8dc38f77ecc2e5
Andres Acevedo <andresca6@gmail.com>
2018-12-07 11:45:10 -0500

    Inicio modulo de calendario

--- src/Controller/Calendario/CalendarioController.php
+++ src/Controller/Calendario/CalendarioController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Calendario;
 
 use App\Entity\Cartera\CarCliente;
+use App\Entity\General\GenEvento;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,22 +14,61 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class CalendarioController extends Controller
 {
-   /**
-    * @Route("/calendario/lista", name="calendario_lista")
-    */    
+    /**
+     * @Route("/calendario/lista", name="calendario_lista")
+     */
     public function lista(Request $request)
     {
         return $this->render('calendario/calendar.html.twig');
     }
 
     /**
+     * @param Request $request
      * @return JsonResponse
      * @Route("/calendario/guardar/evento/", name="calendario_guardar_evento")
      */
-    public function guardarEvento(Request $request){
+    public function guardarEvento(Request $request)
+    {
         $em = $this->getDoctrine()->getManager();
-        $responseArray = [];
-        return new JsonResponse($responseArray);
+        $arrEvento['icono'] = $request->query->get('icono');
+        $arrEvento['titulo'] = $request->query->get('titulo');
+        $arrEvento['desde'] = $request->query->get('desde');
+        $arrEvento['hasta'] = $request->query->get('hasta');
+        $arrEvento['descripcion'] = $request->query->get('descripcion');
+        $arrEvento['color'] = $request->query->get('color');
+
+        $arEvento = new GenEvento();
+        $arEvento->setIcono($arrEvento['icono']);
+        $arEvento->setTitulo($arrEvento['titulo']);
+        $arEvento->setFechaDesde(date_create($arrEvento['desde']));
+        $arEvento->setFechaHasta(date_create($arrEvento['hasta']));
+        $arEvento->setDescripcion($arrEvento['descripcion']);
+        $arEvento->setColor($arrEvento['color']);
+        $arEvento->setUsuario($this->getUser()->getUsername());
+        $em->persist($arEvento);
+
+        $respuesta = true;
+        try {
+            $em->flush();
+        } catch (\Exception $exception) {
+            if ($exception) {
+                dump($exception);
+                die();
+            }
+        }
+        return new JsonResponse($respuesta);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @Route("/calendario/listar/evento/", name="calendario_listar_evento")
+     */
+    public function cargarEventos(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $arEventos = $em->getRepository(GenEvento::class)->jsonEventos($this->getUser()->getUsername());
+        return new JsonResponse($arEventos);
     }
 }
 
---
+++ src/Entity/General/GenEvento.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity\General;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\General\GenEventoRepository")
+ */
+class GenEvento
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", name="codigo_evento_pk")
+     */
+    private $codigoEventoPk;
+
+    /**
+     * @ORM\Column(type="string", length=20, name="icono",nullable =true)
+     */
+    private $icono;
+
+    /**
+     * @ORM\Column(type="string", length=20, name="titulo",nullable=true)
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="datetime", name="desde", nullable=true)
+     */
+    private $fechaDesde;
+
+    /**
+     * @ORM\Column(type="datetime", name="hasta", nullable=true)
+     */
+    private $fechaHasta;
+
+    /**
+     * @ORM\Column(type="string", length=20, name="descripcion",nullable =true)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\Column(type="string", length=100, name="color",nullable =true)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=20, name="usuario",nullable =true)
+     */
+    private $usuario;
+
+    /**
+     * @return mixed
+     */
+    public function getCodigoEventoPk()
+    {
+        return $this->codigoEventoPk;
+    }
+
+    /**
+     * @param mixed $codigoEventoPk
+     */
+    public function setCodigoEventoPk($codigoEventoPk): void
+    {
+        $this->codigoEventoPk = $codigoEventoPk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIcono()
+    {
+        return $this->icono;
+    }
+
+    /**
+     * @param mixed $icono
+     */
+    public function setIcono($icono): void
+    {
+        $this->icono = $icono;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * @param mixed $titulo
+     */
+    public function setTitulo($titulo): void
+    {
+        $this->titulo = $titulo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaDesde()
+    {
+        return $this->fechaDesde;
+    }
+
+    /**
+     * @param mixed $fechaDesde
+     */
+    public function setFechaDesde($fechaDesde): void
+    {
+        $this->fechaDesde = $fechaDesde;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaHasta()
+    {
+        return $this->fechaHasta;
+    }
+
+    /**
+     * @param mixed $fechaHasta
+     */
+    public function setFechaHasta($fechaHasta): void
+    {
+        $this->fechaHasta = $fechaHasta;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * @param mixed $descripcion
+     */
+    public function setDescripcion($descripcion): void
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color): void
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param mixed $usuario
+     */
+    public function setUsuario($usuario): void
+    {
+        $this->usuario = $usuario;
+    }
+}

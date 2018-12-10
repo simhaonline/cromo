838991ab2ffd4e78410bd769177f1d6581ead810
alexsrc <alexcebav@gmail.com>
2018-12-10 08:32:36 -0500

    feat(social avances):

---
+++ src/Controller/Social/InicioController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Social;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InicioController extends AbstractController
+{
+    /**
+     * @Route("/social/general/inicio", name="social_general_inicio")
+     */
+    public function index()
+    {
+        return $this->render('social/inicio.html.twig');
+    }
+}

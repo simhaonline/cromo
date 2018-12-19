beafa33f0258b7d21bd9ea44179c263dcf6fc98e
Andres Acevedo <andresca6@gmail.com>
2018-12-19 14:56:52 -0500

    Adelanto Dashboard

--- src/Repository/General/GenTareaRepository.php
+++ src/Repository/General/GenTareaRepository.php
@@ -13,4 +13,21 @@ class GenTareaRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, GenTarea::class);
     }
+
+    public function lista($usuario){
+        return $this->_em->createQueryBuilder()->from(GenTarea::class,'t')
+            ->select('t.codigoTareaPk')
+            ->addSelect('t.titulo')
+            ->addSelect('tp.nombre as nombrePrioridad')
+            ->addSelect('tp.icono')
+            ->addSelect('t.descripcion')
+            ->addSelect('t.estadoTerminado')
+            ->addSelect('t.fecha')
+            ->addSelect('t.codigoTareaPrioridadFk')
+            ->addSelect('t.usuarioAsigna')
+            ->leftJoin('t.tareaPrioridadRel','tp')
+            ->where("t.usuarioAsigna = '{$usuario}'")
+            ->orderBy('t.codigoTareaPrioridadFk','ASC')
+            ->getQuery()->execute();
+    }
 }

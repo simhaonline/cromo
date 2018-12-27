96f5d800618523fefc8d0382ceca1d3dc0c41b47
Andres Acevedo <andresca6@gmail.com>
2018-12-27 14:27:04 -0500

    feat(Informe desembarco): Se agrega informe de desembarco
    BREAKING CHANGE: T:N/A

--- src/Repository/Transporte/TteDesembarcoRepository.php
+++ src/Repository/Transporte/TteDesembarcoRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\Transporte;
 use App\Entity\Transporte\TteDesembarco;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class TteDesembarcoRepository extends ServiceEntityRepository
 {
@@ -13,5 +14,24 @@ class TteDesembarcoRepository extends ServiceEntityRepository
         parent::__construct($registry, TteDesembarco::class);
     }
 
+    public function lista()
+    {
+        $session = new Session();
+        $qb = $this->_em->createQueryBuilder()
+            ->select('d.codigoDesembarcoPk')
+            ->addSelect('d.codigoDespachoFk')
+            ->addSelect('d.codigoGuiaFk')
+            ->addSelect('d.fecha')
+            ->from(TteDesembarco::class, 'd')
+            ->where('d.codigoDesembarcoPk <> 0');
+        if ($session->get('filtroTteDesembarcoFiltrarFecha')) {
+            if ($session->get('filtroTteDesembarcoFechaDesde')) {
+                $qb->andWhere("d.fecha >= '{$session->get('filtroTteDesembarcoFechaDesde')} 00:00:00'");
+            }
+            if ($session->get('filtroTteDesembarcoFechaHasta')) {
+                $qb->andWhere("d.fecha <= '{$session->get('filtroTteDesembarcoFechaHasta')} 23:59:59'");
+            }
+        }
+        return $qb;
+    }
 }
-

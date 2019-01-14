59273425c439f7b25507226cf874e053b28ecac1
alexsrc <alexcebav@gmail.com>
2019-01-14 08:51:09 -0500

    tarea 92, avance

--- src/Controller/Cartera/Proceso/Contabilidad/CrearReciboMasivoController.php
+++ src/Controller/Cartera/Proceso/Contabilidad/CrearReciboMasivoController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Cartera\Proceso\Contabilidad;
 
+use App\Entity\Cartera\CarCuentaCobrarTipo;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 //use Symfony\Component\HttpKernel\Tests\Controller;
@@ -18,9 +21,25 @@ class CrearReciboMasivoController extends Controller
         $em=$this->getDoctrine()->getManager();
         $session=new Session();
         $paginator  = $this->get('knp_paginator');
+        $form = $this->createFormBuilder()
+            ->add('cboCuentaCobrarTipoRel',EntityType::class,$em->getRepository(CarCuentaCobrarTipo::class)->llenarCombo())
+            ->add('btnFiltrar',SubmitType::class,['label' => "Filtro", 'attr' => ['class' => 'filtrar btn btn-default btn-sm', 'style' => 'float:right']])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('btnFiltrar')->isClicked()) {
+                $arCuentaCobrarTipo = $form->get('cboCuentaCobrarTipoRel')->getData();
+                if ($arCuentaCobrarTipo) {
+                    $session->set('filtroCarCuentaCobrarTipo', $arCuentaCobrarTipo->getCodigoCuentaCobrarTipoPk());
+                } else {
+                    $session->set('filtroCarCuentaCobrarTipo', null);
+                }
+            }
+        }
         $arCrearReciboMasivos=$paginator->paginate($em->getRepository('App:Cartera\CarCuentaCobrar')->crearReciboMasivoLista(), $request->query->getInt('page', 1),100);
         return $this->render('cartera/proceso/contabilidad/crearrecibomasivo/lista.html.twig', [
             'arCrearReciboMasivos' => $arCrearReciboMasivos,
+            'form'                 => $form->createView()
         ]);
     }
 }

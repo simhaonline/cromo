a009d01eca75b6e490a30208b8ec87ccee1c658d
Andres Acevedo <andresca6@gmail.com>
2019-01-21 16:42:37 -0500

    Utilidad importar guias

--- src/Controller/Transporte/Utilidad/Transporte/ImportarGuiaController.php
+++ src/Controller/Transporte/Utilidad/Transporte/ImportarGuiaController.php
@@ -14,12 +14,11 @@ use App\Entity\Transporte\TteServicio;
 use App\Utilidades\Mensajes;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Tests\Matcher\DumpedUrlMatcherTest;
-
 
 class ImportarGuiaController extends Controller
 {
@@ -34,17 +33,20 @@ class ImportarGuiaController extends Controller
         $em = $this->getDoctrine()->getManager();
         $paginator = $this->get('knp_paginator');
         $form = $this->createFormBuilder()
-            ->add('fechaIngresoDesde', DateType::class, ['widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => ['class' => 'date form-control',], 'required' => false])
-            ->add('fechaIngresoHasta', DateType::class, ['widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => ['class' => 'date form-control',], 'required' => false])
+            ->add('fechaIngresoDesde', DateType::class, ['widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => ['class' => 'date form-control',], 'required' => false,'data' => $session->get('filtroGuiaFechaIngresoDesde') != '' ? date_create($session->get('filtroGuiaFechaIngresoDesde')) : null ])
+            ->add('fechaIngresoHasta', DateType::class, ['widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => ['class' => 'date form-control',], 'required' => false,'data' => $session->get('filtroGuiaFechaIngresoHasta') != '' ? date_create($session->get('filtroGuiaFechaIngresoHasta')) : null ])
+            ->add('codigoClienteFk', TextType::class, ['required' => false,'data' => $session->get('filtroGuiaCodigoCliente')])
             ->add('btnFiltrar', SubmitType::class, ['label' => "Filtro", 'attr' => ['class' => 'filtrar btn btn-default btn-sm', 'style' => 'float:right']])
             ->add('btnImportar', SubmitType::class, ['label' => "Importar", 'attr' => ['class' => 'filtrar btn btn-default btn-sm', 'style' => 'float:right']])
+            ->add('btnMarcar', SubmitType::class, ['label' => "Marcar exportado", 'attr' => ['class' => 'filtrar btn btn-default btn-sm', 'style' => 'float:right']])
             ->getForm();
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             if ($form->get('btnFiltrar')->isClicked()) {
                 $em->getRepository(TteGuiaTemporal::class)->createQueryBuilder('t')->delete(TteGuiaTemporal::class)->getQuery()->execute();
-                $session->set('filtroGuiaFechaIngresoDesde', $form->get('fechaIngresoDesde')->getData() ? $form->get('fechaIngresoDesde')->getData()->format('Y-m-d') : null);
-                $session->set('filtroGuiaFechaIngresoHasta', $form->get('fechaIngresoHasta')->getData() ? $form->get('fechaIngresoHasta')->getData()->format('Y-m-d') : null);
+                $session->set('filtroGuiaFechaIngresoDesde', $form->get('fechaIngresoDesde')->getData() ? $form->get('fechaIngresoDesde')->getData()->format('Y-m-d') : '');
+                $session->set('filtroGuiaFechaIngresoHasta', $form->get('fechaIngresoHasta')->getData() ? $form->get('fechaIngresoHasta')->getData()->format('Y-m-d') : '');
+                $session->set('filtroGuiaCodigoCliente', $form->get('codigoClienteFk')->getData() ?? '');
                 $this->pendiente();
             }
             if ($form->get('btnImportar')->isClicked()) {
@@ -54,25 +56,42 @@ class ImportarGuiaController extends Controller
                 }
                 $em->getRepository(TteGuiaTemporal::class)->createQueryBuilder('t')->delete(TteGuiaTemporal::class)->getQuery()->execute();
             }
+            if($form->get('btnMarcar')->isClicked()){
+                $arrSeleccionados = $request->request->get('ChkSeleccionar');
+                if($arrSeleccionados){
+                    $this->marcarExportado($arrSeleccionados);
+                }
+                $em->getRepository(TteGuiaTemporal::class)->createQueryBuilder('t')->delete(TteGuiaTemporal::class)->getQuery()->execute();
+            }
             return $this->redirect($this->generateUrl('transporte_utilidad_transporte_importar_guia'));
         }
         $arGuias = $paginator->paginate($em->getRepository(TteGuiaTemporal::class)->findAll(), $request->query->getInt('page', 1), 30);
         return $this->render('transporte/utilidad/transporte/importarGuia/importarGuia.html.twig', [
-            'form' => $form->createView(),
+            'formFiltro' => $form->createView(),
             'arGuias' => $arGuias
         ]);
     }
 
     private function pendiente()
     {
+        $session = new Session();
         $em = $this->getDoctrine()->getManager();
         $arConfiguracion = $em->find(GenConfiguracion::class, 1);
         $arConfiguracionTransporte = $em->find(TteConfiguracion::class, 1);
         $url = $arConfiguracion->getWebServiceGalioUrl() . '/api/pendientes/guia/' . $arConfiguracionTransporte->getCodigoOperadorFk();
+        $arrDatos['nit'] = $em->find(TteCliente::class,$session->get('filtroGuiaCodigoCliente'))->getNumeroIdentificacion();
+        $arrDatos['fechaHasta'] = $session->get('filtroGuiaFechaIngresoHasta');
+        $arrDatos['fechaDesde'] = $session->get('filtroGuiaFechaIngresoDesde');
+        $arrDatos = json_encode($arrDatos);
         $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $arrDatos);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($arrDatos)]);
         $arrGuias = json_decode(curl_exec($ch));
+
         if ($arrGuias) {
             foreach ($arrGuias as $arrGuia) {
                 $arCliente = $em->getRepository(TteCliente::class)->findOneBy(['numeroIdentificacion' => $arrGuia->nit]);
@@ -83,7 +102,7 @@ class ImportarGuiaController extends Controller
                 $arGuiaTemporal->setOperacion($arrGuia->operacion);
                 $arGuiaTemporal->setCodigoGuiaTipoFk($arrGuia->codigoGuiaTipoFk);
                 $arGuiaTemporal->setClienteRel($arCliente);
-                $arGuiaTemporal->setFechaIngreso(date_create($arrGuia->fechaIngreso));
+                $arGuiaTemporal->setFechaIngreso(date_create($arrGuia->fechaIngreso->date));
                 $arGuiaTemporal->setClienteDocumento($arrGuia->clienteDocumento);
                 $arGuiaTemporal->setDestinatarioNombre($arrGuia->destinatarioNombre);
                 $arGuiaTemporal->setCiudadOrigenRel($arCiudadOrigen);
@@ -153,6 +172,33 @@ class ImportarGuiaController extends Controller
             Mensajes::success('Guias exportadas correctamente');
         } else {
             Mensajes::error('Ha ocurrido un error al momento de exportar las guias');
+        }
+    }
+
+    private function marcarExportado($arrNumeros){
+        $em = $this->getDoctrine()->getManager();
+        $arConfiguracion = $em->find(GenConfiguracion::class, 1);
+        $arConfiguracionTransporte = $em->find(TteConfiguracion::class, 1);
+        $url = $arConfiguracion->getWebServiceGalioUrl() . '/api/importar/guia';
+        foreach ($arrNumeros as $codigoGuiaTemporal){
+            $arrNumeros[] = $em->find(TteGuiaTemporal::class,$codigoGuiaTemporal)->getNumero();
+        }
+        $arrDatos['numeros'] = $arrNumeros;
+        $arrDatos['codigoOperador'] = $arConfiguracionTransporte->getCodigoOperadorFk();
+        $arrDatos = json_encode($arrDatos);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $arrDatos);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($arrDatos)]);
+        $respuesta = curl_exec($ch);
+        curl_close($ch);
+        if ($respuesta) {
+            Mensajes::success('Guias marcadas como exportadas correctamente');
+        } else {
+            Mensajes::error('Ha ocurrido un error al momento de marcar las guias');
         }
     }
 }

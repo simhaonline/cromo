a249858b2e800a3936dc343690c44d7b429542bb
Andres Acevedo <andresca6@gmail.com>
2019-01-23 08:26:36 -0500

    fix(Boton guardar): Se corrige error al momento de crear una nueva cuenta
    BREAKING CHANGE: D:105

--- src/Form/Type/General/CuentaType.php
+++ src/Form/Type/General/CuentaType.php
@@ -7,6 +7,7 @@ use App\Entity\General\GenCuenta;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,19 +17,21 @@ class CuentaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('codigoCuentaPk', TextType::class, ['required' => true])
-            ->add('nombre', TextType::class, ['required' => true])
-            ->add('cuenta', TextType::class, ['required' => true])
-            ->add('tipo', TextType::class, ['required' => true])
-            ->add('codigoCuentaContableFk', TextType::class, ['required' => true])
+            ->add('codigoCuentaPk', TextType::class, ['required' => true, 'label' => 'Código:'])
+            ->add('nombre', TextType::class, ['required' => true, 'label' => 'Nombre:'])
+            ->add('cuenta', TextType::class, ['required' => true,'label' => 'Cuenta:'])
+            ->add('tipo', TextType::class, ['required' => true,'label' => 'Tipo:'])
+            ->add('codigoCuentaContableFk', TextType::class, ['required' => true,'label' => 'Cuenta contable:'])
             ->add('bancoRel',EntityType::class,[
                 'class' => GenBanco::class,
                 'query_builder' => function(EntityRepository $er){
                     return $er->createQueryBuilder('er')
                         ->orderBy('er.nombre');
                 },'choice_label' => 'nombre',
-                'required' => true
-            ]);
+                'required' => true,
+                'label' => 'Banco:'
+            ])
+            ->add('guardar',SubmitType::class,['label' => 'Guardar','attr' => ['class' => 'btn btn-sm btn-primary']]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

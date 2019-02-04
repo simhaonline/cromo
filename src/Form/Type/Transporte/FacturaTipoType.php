4d159b81ff6cad7bfb85e8165c887f6b2978d8c2
sebas957 <baxtian957@hotmail.com>
2019-02-04 11:48:44 -0500

    Ajuste a factura tipo. Se eliminan campos del form que ya no estan en dicha parte

--- src/Form/Type/Transporte/FacturaTipoType.php
+++ src/Form/Type/Transporte/FacturaTipoType.php
@@ -24,18 +24,13 @@ class FacturaTipoType extends AbstractType
             ->add('guiaFactura', CheckboxType::class, array('required'  => false,'label'=>'Guia factura'))
             ->add('codigoCuentaCobrarTipoFk',TextType::class,['required' => false,'label' => 'Codigo cuenta cobrar tipo fk:'])
             ->add('codigoFacturaClaseFk',TextType::class,['required' => false,'label' => 'Codigo factura clase fk:'])
-            ->add('codigoCuentaIngresoFleteFk',TextType::class,['required' => false,'label' => 'Codigo cuenta ingreso flete fk:'])
-            ->add('codigoCuentaIngresoTerceroFk',TextType::class,['required' => false,'label' => 'Codigo cuenta ingreso flete tercero fk:'])
-            ->add('codigoCuentaIngresoManejoFk',TextType::class,['required' => false,'label' => 'Codigo cuenta ingreso manejo fk:'])
             ->add('codigoCuentaClienteFk',TextType::class,['required' => false,'label' => 'Codigo cuenta cliente fk:'])
             ->add('codigoCuentaIngresoFleteIntermediacionFk',TextType::class,['required' => false,'label' => 'Codigo cuenta ingreso flete (Intermediacion) :'])
-            ->add('naturalezaCuentaIngresoTercero',TextType::class,['required' => false,'label' => 'Naturaleza cuenta ingreso:'])
             ->add('naturalezaCuentaIngreso',TextType::class,['required' => false,'label' => 'Naturaleza cuenta ingreso:'])
             ->add('naturalezaCuentaCliente',TextType::class,['required' => false,'label' => 'Naturaleza cuenta cliente:'])
             ->add('naturalezaCuentaIngresoFleteIntermediacion',TextType::class,['required' => false,'label' => 'Naturaleza cuenta ingreso flete (intermediacion):'])
             ->add('codigoComprobanteFk',TextType::class,['required' => false,'label' => 'Codigo comprobante:'])
             ->add('guardar', SubmitType::class, ['label'=>'Guardar','attr' => ['class' => 'btn btn-sm btn-primary']])
-            ->add('guardarnuevo', SubmitType::class, ['label'=>'Guardar y nuevo','attr' => ['class' => 'btn btn-sm btn-primary']]);;
         ;
     }
 

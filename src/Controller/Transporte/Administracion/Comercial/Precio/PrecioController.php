dc5e00dfd7af1848f3d83a4721f60a8df5d91786
sebas957 <baxtian957@hotmail.com>
2019-02-13 15:19:51 -0500

    Se crea funcionalidad de carga masiva de precios detalle. Se crea el instructivo de importacion de precios

--- src/Controller/Transporte/Administracion/Comercial/Precio/PrecioController.php
+++ src/Controller/Transporte/Administracion/Comercial/Precio/PrecioController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Transporte\Administracion\Comercial\Precio;
 
+use App\Entity\General\GenConfiguracion;
+use App\Entity\Transporte\TteCiudad;
+use App\Entity\Transporte\TteProducto;
 use App\General\General;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Session\Session;
 use App\Controller\Estructura\ControllerListenerGeneral;
@@ -143,6 +148,135 @@ class PrecioController extends ControllerListenerGeneral
             }
         }
         return $this->render('transporte/administracion/comercial/precio/detalleNuevo.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     * @Route("/transporte/administracion/comercial/precio/importar/{codigoPrecio}", name="transporte_administracion_comercial_precio_importar")
+     */
+    public function importarPrecios(Request $request, $codigoPrecio)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $arPrecio = $em->getRepository(TtePrecio::class)->find($codigoPrecio);
+        $form = $this->createFormBuilder()
+            ->add('flArchivo', FileType::class)
+            ->add('btnImportarPrecio', SubmitType::class, ['label' => 'Importar', 'attr' => ['class' => 'btn btn-sm btn-default']])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('btnImportarPrecio')->isClicked()) {
+                $ruta = $em->getRepository(GenConfiguracion::class)->parametro('rutaTemporal');
+                //$ruta = "/var/www/temporal/";
+                if (!$ruta) {
+                    Mensajes::error('Debe de ingresar una ruta temporal en la configuracion general del sistema');
+                    echo "<script language='Javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
+                }
+                $form['flArchivo']->getData()->move($ruta, "archivo.xls");
+                $rutaArchivo = $ruta . "archivo.xls";
+                $reader = \PhpOffice\PhpSpreadsheet\IOFactory::load($rutaArchivo);
+                $arrCargas = [];
+                $i = 0;
+                if ($reader->getSheetCount() > 1) {
+                    Mensajes::error('El documento debe contener solamente una hoja');
+                    echo "<script language='Javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
+                } else {
+                    foreach ($reader->getWorksheetIterator() as $worksheet) {
+                        $highestRow = $worksheet->getHighestRow();
+                        for ($row = 2; $row <= $highestRow; ++$row) {
+                            $cell = $worksheet->getCellByColumnAndRow(3, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['codigoCiudadOrigenFk'] = $cell->getValue();
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(5, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['codigoCiudadDestinoFk'] = $cell->getValue();
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(7, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['codigoProductoFk'] = $cell->getValue();
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(8, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['vrPeso'] = $cell->getValue();
+                            } else {
+                                $arrCargas [$i]['vrPeso'] = 0;
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(9, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['vrUnidad'] = $cell->getValue();
+                            } else {
+                                $arrCargas [$i]['vrUnidad'] = 0;
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(10, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['pesoTope'] = $cell->getValue();
+                            } else {
+                                $arrCargas [$i]['pesoTope'] = 0;
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(11, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['vrPesoTope'] = $cell->getValue();
+                            } else {
+                                $arrCargas [$i]['vrPesoTope'] = 0;
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(12, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['vrPesoTopeAdicional'] = $cell->getValue();
+                            } else {
+                                $arrCargas [$i]['vrPesoTopeAdicional'] = 0;
+                            }
+                            $cell = $worksheet->getCellByColumnAndRow(13, $row);
+                            if ($cell->getValue() != '') {
+                                $arrCargas [$i]['minimo'] = $cell->getValue();
+                            } else {
+                                $arrCargas [$i]['minimo'] = 0;
+                            }
+                            $i++;
+                        }
+                    }
+                    //leercargas
+                    foreach ($arrCargas as $arrCarga) {
+                        $arPrecioDetalle = $em->getRepository(TtePrecioDetalle::class)->findOneBy(array('codigoPrecioFk' => $codigoPrecio, 'codigoCiudadOrigenFk' => $arrCarga['codigoCiudadOrigenFk'], 'codigoCiudadDestinoFk' => $arrCarga['codigoCiudadDestinoFk'], 'codigoProductoFk' => $arrCarga['codigoProductoFk']));
+                        if ($arPrecioDetalle) {
+                            $arPrecioDetalle->setVrPeso($arrCarga['vrPeso']);
+                            $arPrecioDetalle->setVrUnidad($arrCarga['vrUnidad']);
+                            $arPrecioDetalle->setPesoTope($arrCarga['pesoTope']);
+                            $arPrecioDetalle->setVrPesoTope($arrCarga['vrPesoTope']);
+                            $arPrecioDetalle->setVrPesoTopeAdicional($arrCarga['vrPesoTopeAdicional']);
+                            $arPrecioDetalle->setMinimo($arrCarga['minimo']);
+                            $em->persist($arPrecioDetalle);
+                        } else {
+                            if ($arrCarga['codigoCiudadOrigenFk'] && $arrCarga['codigoCiudadDestinoFk'] && $arrCarga['codigoProductoFk']) {
+                                $arCiudadOrigen = $em->getRepository(TteCiudad::class)->find($arrCarga['codigoCiudadOrigenFk']);
+                                $arCiudadDestino = $em->getRepository(TteCiudad::class)->find($arrCarga['codigoCiudadDestinoFk']);
+                                $arProducto = $em->getRepository(TteProducto::class)->find($arrCarga['codigoProductoFk']);
+                                if ($arCiudadOrigen && $arCiudadDestino && $arProducto) {
+                                    $arPrecioDetalleNuevo = new TtePrecioDetalle();
+                                    $arPrecioDetalleNuevo->setPrecioRel($arPrecio);
+                                    $arPrecioDetalleNuevo->setCiudadOrigenRel($arCiudadOrigen);
+                                    $arPrecioDetalleNuevo->setCiudadDestinoRel($arCiudadDestino);
+                                    $arPrecioDetalleNuevo->setProductoRel($arProducto);
+                                    $arPrecioDetalleNuevo->setVrPeso($arrCarga['vrPeso']);
+                                    $arPrecioDetalleNuevo->setVrUnidad($arrCarga['vrUnidad']);
+                                    $arPrecioDetalleNuevo->setPesoTope($arrCarga['pesoTope']);
+                                    $arPrecioDetalleNuevo->setVrPesoTope($arrCarga['vrPesoTope']);
+                                    $arPrecioDetalleNuevo->setVrPesoTopeAdicional($arrCarga['vrPesoTopeAdicional']);
+                                    $arPrecioDetalleNuevo->setMinimo($arrCarga['minimo']);
+                                    $em->persist($arPrecioDetalleNuevo);
+                                }
+                            }
+                        }
+                    }
+                    $em->flush();
+                    echo "<script language='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
+                }
+            }
+        }
+        return $this->render('transporte/administracion/comercial/precio/importarPreciosArchivo.html.twig', [
             'form' => $form->createView()
         ]);
     }

9e1256fba77ad69446cebddac998814ef2b4a58d
sebas957 <baxtian957@hotmail.com>
2019-03-07 14:23:56 -0500

    Se realiza ajuste al formato de lote existencia y a la vista

--- src/Formato/Inventario/ExistenciaLote.php
+++ src/Formato/Inventario/ExistenciaLote.php
@@ -28,7 +28,7 @@ class ExistenciaLote extends \FPDF {
 
     public function Header() {
 
-        Estandares::generarEncabezado($this, 'ORDEN DE COMPRA', self::$em);
+        Estandares::generarEncabezado($this, 'LOTE EXISTENCIA', self::$em);
         $this->SetFillColor(200, 200, 200);
         $this->SetFont('Arial', 'B', 10);
         //Logo
@@ -43,14 +43,14 @@ class ExistenciaLote extends \FPDF {
 
     public function EncabezadoDetalles() {
         $this->Ln(34);
-        $header = array('ITEM', 'NOMBRE','MARCA', 'BODEGA', 'LOTE', 'EXISTENCIA', 'CANT DISPONIBLE');
+        $header = array('ITEM', 'NOMBRE','MARCA', 'BODEGA', 'LOTE', 'EXI', 'REM', 'DIS');
         $this->SetFillColor(170, 170, 170);
         $this->SetTextColor(0);
         $this->SetDrawColor(0, 0, 0);
         $this->SetLineWidth(.2);
         $this->SetFont('arial', 'B', 7);
         //creamos la cabecera de la tabla.
-        $w = array(12, 48, 30, 20, 20, 30, 30);
+        $w = array(12, 48, 30, 20, 30, 15, 15, 15);
         for ($i = 0; $i < count($header); $i++)
             if ($i == 0 || $i == 1)
                 $this->Cell($w[$i], 4, $header[$i], 1, 0, 'C', 1);
@@ -70,12 +70,13 @@ class ExistenciaLote extends \FPDF {
         $pdf->SetFont('Arial', '', 7);
         foreach ($arExistenciaLote as $arExistenciaLote) {
             $pdf->Cell(12, 4, $arExistenciaLote['codigoItemFk'], 1, 0, 'L');
-            $pdf->Cell(48, 4, $arExistenciaLote['itemNombre'], 1, 0, 'L');
+            $pdf->Cell(48, 4, utf8_decode($arExistenciaLote['itemNombre']), 1, 0, 'L');
             $pdf->Cell(30, 4, $arExistenciaLote['marca'], 1, 0, 'L');
             $pdf->Cell(20, 4, $arExistenciaLote['codigoBodegaFk'], 1, 0, 'L');
-            $pdf->Cell(20, 4, $arExistenciaLote['loteFk'], 1, 0, 'L');
-            $pdf->Cell(30, 4, number_format($arExistenciaLote['cantidadExistencia']), 1, 0, 'R');
-            $pdf->Cell(30, 4, number_format($arExistenciaLote['cantidadDisponible']), 1, 0, 'R');
+            $pdf->Cell(30, 4, $arExistenciaLote['loteFk'], 1, 0, 'L');
+            $pdf->Cell(15, 4, number_format($arExistenciaLote['cantidadExistencia']), 1, 0, 'R');
+            $pdf->Cell(15, 4, number_format($arExistenciaLote['cantidadRemisionada']), 1, 0, 'R');
+            $pdf->Cell(15, 4, number_format($arExistenciaLote['cantidadDisponible']), 1, 0, 'R');
             $pdf->Ln();
             $pdf->SetAutoPageBreak(true, 25);
         }

512e9a030a99482394be39e6c447b550a68df97a
wariox3 <maestradaz3@gmail.com>
2019-05-28 14:18:14 -0500

    feat: turnos

---
+++ src/Controller/Turno/Tablero/Gerente/ComercialController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Turno\Tablero\Gerente;
+
+use App\Formato\Despacho;
+use App\Entity\Transporte\TteRecogida;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Ob\HighchartsBundle\Highcharts\Highchart;
+
+class ComercialController extends Controller
+{
+   /**
+    * @Route("/turno/tablero/gerente/comercial", name="turno_tablero_gerente_comercial")
+    */    
+    public function principal(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $fecha = new \DateTime('now');
+        $arrResumenRecogidas = $em->getRepository(TteRecogida::class)->resumenCuenta($fecha->format('Y-m-d'), $fecha->format('Y-m-d'));
+        $arrResumenRecogidasOperacion = $em->getRepository(TteRecogida::class)->resumenOperacion($fecha->format('Y-m-d'), $fecha->format('Y-m-d'));
+
+
+        // Chart
+        $series = array(
+            array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+        );
+
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+        $ob->title->text('Chart Title');
+        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob->series($series);
+
+        return $this->render('transporte/tablero/recogida/recogida.html.twig', [
+            'arrResumenRecogidas' => $arrResumenRecogidas,
+            'arrResumenRecogidasOperacion' => $arrResumenRecogidasOperacion,
+            'chart' => $ob
+            ]);
+    }
+}
+

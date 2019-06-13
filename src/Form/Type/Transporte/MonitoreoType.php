73e2f8adb16d2dcadaa3eb2b8312c5767af38697
afcanop <afcanop@gmail.com>
2019-06-13 08:58:28 -0500

    se agrego el filtro por placa en Transporte->movimiento->monitoreo según la solicitud del cliente.

--- src/Form/Type/Transporte/MonitoreoType.php
+++ src/Form/Type/Transporte/MonitoreoType.php
@@ -40,6 +40,8 @@ class MonitoreoType extends AbstractType
         $campos = '[
             {"child":"fechaInicioDesde",                      "tipo":"DateType",   "propiedades":{"label":"Fecha Desde"}},
             {"child":"fechaFinHasta",                         "tipo":"DateType",   "propiedades":{"label":"Fecha Hasta"}},
+            {"child":"codigoVehiculoFk",                      "tipo":"TextType",   "propiedades":{"label":"Placa"}},
+            {"child":"fechaFinHasta",                         "tipo":"DateType",   "propiedades":{"label":"Fecha Hasta"}},
             {"child":"estadoAutorizado",                      "tipo":"ChoiceType",   "propiedades":{"label":"Autorizado",     "choices":{"SI":true,"NO":false}}},
             {"child":"estadoAprobado",                        "tipo":"ChoiceType",   "propiedades":{"label":"Aprobado",       "choices":{"SI":true,"NO":false}}},
             {"child":"estadoAnulado",                         "tipo":"ChoiceType",   "propiedades":{"label":"Anulado",        "choices":{"SI":true,"NO":false}}}

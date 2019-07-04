2b22e46f872e12e05d99b610799a6d7f6e90b259
Sebas <baxtian957@hotmail.com>
2019-07-04 17:33:12 -0500

    Se crea validacion para que las recogidas programadas no se vayan a duplicar al generar

--- src/Repository/Transporte/TteRecogidaProgramadaRepository.php
+++ src/Repository/Transporte/TteRecogidaProgramadaRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository\Transporte;
 
+use App\Entity\Transporte\TteRecogida;
 use App\Entity\Transporte\TteRecogidaProgramada;
+use App\Utilidades\Mensajes;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -35,7 +37,7 @@ class TteRecogidaProgramadaRepository extends ServiceEntityRepository
      */
     public function eliminar($arrSeleccionados)
     {
-        if($arrSeleccionados) {
+        if ($arrSeleccionados) {
             foreach ($arrSeleccionados as $arrSeleccionado) {
                 $ar = $this->getEntityManager()->getRepository(TteRecogidaProgramada::class)->find($arrSeleccionado);
                 if ($ar) {
@@ -44,6 +46,61 @@ class TteRecogidaProgramadaRepository extends ServiceEntityRepository
             }
             $this->getEntityManager()->flush();
         }
+    }
+
+    public function generarTodos()
+    {
+        $em = $this->getEntityManager();
+        $queryBuilder = $em->createQueryBuilder()->from(TteRecogidaProgramada::class, 'rp')
+            ->select('rp.codigoRecogidaProgramadaPk');
+        $arRecogidasProgramadas = $queryBuilder->getQuery()->getResult();
+        foreach ($arRecogidasProgramadas as $arRecogidasProgramada) {
+            $this->generarRecogidaProgramada($arRecogidasProgramada['codigoRecogidaProgramadaPk']);
+        }
+        $em->flush();
+    }
+
+    public function generarSeleccionados($arrSeleccionados)
+    {
+        $em = $this->getEntityManager();
+        if ($arrSeleccionados) {
+            foreach ($arrSeleccionados as $codigo) {
+                $this->generarRecogidaProgramada($codigo);
+            }
+        }
+        $em->flush();
+    }
+
+    public function generarRecogidaProgramada($codigoRecogidaProgramada)
+    {
+        /**
+         * @var $arRecogidasProgramadas TteRecogidaProgramada
+         */
+        $em = $this->getEntityManager();
+        $arRecogidaProgramada = $em->getRepository(TteRecogidaProgramada::class)->find($codigoRecogidaProgramada);
+        $fecha = new \DateTime('now');
+        $fechaHora = $fecha->format('Y-m-d');
+        $arRecogidaYaProgramada = $em->getRepository(TteRecogida::class)->yaProgramada($fechaHora, $arRecogidaProgramada->getCodigoClienteFk(), $arRecogidaProgramada->getCodigoRutaRecogidaFk());
+        if ($arRecogidaYaProgramada) {
+            Mensajes::error("Ya existe un registro de recogida para esta fecha .$codigoRecogidaProgramada");
+        } else {
+            $arRecogida = new TteRecogida();
+            $fechaRecogida = date_create($fechaHora . " " . $arRecogidaProgramada->getHora()->format('H:i'));
+            $arRecogida->setFechaRegistro(new \DateTime('now'));
+            $arRecogida->setFecha($fechaRecogida);
+            $arRecogida->setClienteRel($arRecogidaProgramada->getClienteRel());
+            $arRecogida->setRutaRecogidaRel($arRecogidaProgramada->getRutaRecogidaRel());
+            $arRecogida->setOperacionRel($arRecogidaProgramada->getOperacionRel());
+            $arRecogida->setCiudadRel($arRecogidaProgramada->getCiudadRel());
+            $arRecogida->setAnunciante($arRecogidaProgramada->getAnunciante());
+            $arRecogida->setDireccion($arRecogidaProgramada->getDireccion());
+            $arRecogida->setTelefono($arRecogidaProgramada->getTelefono());
+            $arRecogida->setComentario($arRecogidaProgramada->getComentario());
+            $arRecogida->setEstadoAutorizado(1);
+            $arRecogida->setEstadoAprobado(1);
+            $em->persist($arRecogida);
+        }
+        $em->flush();
     }
 
 }

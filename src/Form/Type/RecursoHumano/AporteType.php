ad91601d81addd68a9e6ce89d992c6e2c63705d2
afcanop <afcanop@gmail.com>
2019-07-04 13:17:38 -0500

    Crear crud movimiento de recurso humano de la entidad RhuAporte solo propiedades año y mes.

---
+++ src/Form/Type/RecursoHumano/AporteType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form\Type\RecursoHumano;
+
+use App\Entity\RecursoHumano\RhuAporte;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AporteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('anio', TextType::class, array('required' => true))
+            ->add('mes', IntegerType::class, array('required' => true))
+            ->add('guardar',SubmitType::class,['label' => 'Guardar','attr' => ['class' => 'btn btn-sm btn-primary']]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => RhuAporte::class,
+        ]);
+    }
+
+}

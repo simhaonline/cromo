b682209d4c84416dd7102c0fc5eec4657750579e
afcanop <afcanop@gmail.com>
2019-08-11 14:05:44 -0500

    validaciones de licencia

--- src/Controller/RecursoHumano/Movimiento/Nomina/Licencia/LicenciaController.php
+++ src/Controller/RecursoHumano/Movimiento/Nomina/Licencia/LicenciaController.php
@@ -5,6 +5,7 @@ namespace App\Controller\RecursoHumano\Movimiento\Nomina\Licencia;
 
 
 use App\Controller\Estructura\ControllerListenerGeneral;
+use App\Entity\RecursoHumano\RhuAporteDetalle;
 use App\Entity\RecursoHumano\RhuContrato;
 use App\Entity\RecursoHumano\RhuEmpleado;
 use App\Entity\RecursoHumano\RhuGrupo;
@@ -62,37 +63,37 @@ class LicenciaController extends ControllerListenerGeneral
                 'choice_label' => 'nombre',
                 'placeholder' => 'TODOS'
             ])
-            ->add('licenciaFechaDesde', DateType::class, ['label' => 'Fecha desde: ',  'required' => false, 'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'data' => $session->get('filtroRhuLicenciaFechaDesde') ? date_create($session->get('filtroRhuLicenciaFechaDesde')): null])
-            ->add('licenciafechaHasta', DateType::class, ['label' => 'Fecha hasta: ', 'required' => false,  'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'data' => $session->get('filtroRhuLicenciaFechaHasta') ? date_create($session->get('filtroRhuLicenciaFechaHasta')): null])
+            ->add('licenciaFechaDesde', DateType::class, ['label' => 'Fecha desde: ', 'required' => false, 'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'data' => $session->get('filtroRhuLicenciaFechaDesde') ? date_create($session->get('filtroRhuLicenciaFechaDesde')) : null])
+            ->add('licenciafechaHasta', DateType::class, ['label' => 'Fecha hasta: ', 'required' => false, 'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'data' => $session->get('filtroRhuLicenciaFechaHasta') ? date_create($session->get('filtroRhuLicenciaFechaHasta')) : null])
             ->add('btnFiltro', SubmitType::class, array('label' => 'Filtrar'))
-            ->add( 'btnEliminar', SubmitType::class, ['label'=>'Eliminar', 'attr'=>['class'=> 'btn btn-sm btn-danger']])
+            ->add('btnEliminar', SubmitType::class, ['label' => 'Eliminar', 'attr' => ['class' => 'btn btn-sm btn-danger']])
             ->getForm();
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
             if ($form->get('btnFiltro')->isClicked()) {
-                $codigoEmpleado =$form->get('codigoEmpleadoPk')->getData();
-                $arGrupo =$form->get('grupoRel')->getData();
-                $arLicenciaTipo =$form->get('licenciaTipo')->getData();
-                $arEmpleado = $em->getRepository(RhuEmpleado::class)->find($codigoEmpleado??0);
-                if ($arEmpleado){
-                    $session->set('filtroRhuLicenciaCodigoEmpleado',  $arEmpleado->getCodigoEmpleadoPk());
-                }else{
-                    $session->set('filtroRhuLicenciaCodigoEmpleado',  null);
+                $codigoEmpleado = $form->get('codigoEmpleadoPk')->getData();
+                $arGrupo = $form->get('grupoRel')->getData();
+                $arLicenciaTipo = $form->get('licenciaTipo')->getData();
+                $arEmpleado = $em->getRepository(RhuEmpleado::class)->find($codigoEmpleado ?? 0);
+                if ($arEmpleado) {
+                    $session->set('filtroRhuLicenciaCodigoEmpleado', $arEmpleado->getCodigoEmpleadoPk());
+                } else {
+                    $session->set('filtroRhuLicenciaCodigoEmpleado', null);
                 }
-                if ($arLicenciaTipo){
-                    $session->set('filtroRhuLicenciaLiencenciaTipo',  $arLicenciaTipo->getCodigoLicenciaTipoPk());
-                }else{
-                    $session->set('filtroRhuLicenciaLiencenciaTipo',  null);
+                if ($arLicenciaTipo) {
+                    $session->set('filtroRhuLicenciaLiencenciaTipo', $arLicenciaTipo->getCodigoLicenciaTipoPk());
+                } else {
+                    $session->set('filtroRhuLicenciaLiencenciaTipo', null);
                 }
-                if ($arGrupo){
-                    $session->set('filtroRhuLicenciaCodigoGrupo',  $arGrupo->getCodigoGrupoPk());
-                }else{
-                    $session->set('filtroRhuLicenciaCodigoGrupo',  null);
+                if ($arGrupo) {
+                    $session->set('filtroRhuLicenciaCodigoGrupo', $arGrupo->getCodigoGrupoPk());
+                } else {
+                    $session->set('filtroRhuLicenciaCodigoGrupo', null);
                 }
-                $session->set('filtroRhuLicenciaFechaDesde',  $form->get('licenciaFechaDesde')->getData() ?$form->get('licenciaFechaDesde')->getData()->format('Y-m-d'): null);
-                $session->set('filtroRhuLicenciaFechaHasta', $form->get('licenciafechaHasta')->getData() ? $form->get('licenciafechaHasta')->getData()->format('Y-m-d'): null);
+                $session->set('filtroRhuLicenciaFechaDesde', $form->get('licenciaFechaDesde')->getData() ? $form->get('licenciaFechaDesde')->getData()->format('Y-m-d') : null);
+                $session->set('filtroRhuLicenciaFechaHasta', $form->get('licenciafechaHasta')->getData() ? $form->get('licenciafechaHasta')->getData()->format('Y-m-d') : null);
             }
-            if ($form->get('btnEliminar')->isClicked()){
+            if ($form->get('btnEliminar')->isClicked()) {
                 $arLicenicasSeleccionados = $request->request->get('ChkSeleccionar');
                 $this->get("UtilidadesModelo")->eliminar(RhuLicencia::class, $arLicenicasSeleccionados);
                 return $this->redirect($this->generateUrl('recursohumano_movimiento_nomina_licencia_lista'));
@@ -126,53 +127,126 @@ class LicenciaController extends ControllerListenerGeneral
         if ($form->isSubmitted() && $form->isValid()) {
             if ($form->get('guardar')->isClicked()) {
                 $codigoEmpleado = $form->get('codigoEmpleadoFk')->getData();
-                $arEmpleado = $em->getRepository(RhuEmpleado::class)->findOneBy(['codigoEmpleadoPk'=>$codigoEmpleado]);
-                $arContrato = $em->getRepository(RhuContrato::class)->findOneBy(['codigoEmpleadoFk'=>$arEmpleado->getCodigoEmpleadoPk()??0]);
+                $arEmpleado = $em->getRepository(RhuEmpleado::class)->findOneBy(['codigoEmpleadoPk' => $codigoEmpleado]);
+                $arContrato = $em->getRepository(RhuContrato::class)->findOneBy(['codigoEmpleadoFk' => $arEmpleado->getCodigoEmpleadoPk() ?? 0]);
                 $arLicencia = $form->getData();
-                if ($arEmpleado){
-                    if ($arContrato){
+                if ($arEmpleado) {
+                    if ($arContrato) {
                         if ($arLicencia->getFechaDesde() <= $arLicencia->getFechaHasta()) {
                             if ($em->getRepository(RhuIncapacidad::class)->validarFecha($arLicencia->getFechaDesde(), $arLicencia->getFechaHasta(), $arEmpleado->getCodigoEmpleadoPk(), "")) {
                                 if ($em->getRepository(RhuLicencia::class)->validarFecha($arLicencia->getFechaDesde(), $arLicencia->getFechaHasta(), $arEmpleado->getCodigoEmpleadoPk(), $arLicencia->getCodigoLicenciaPk())) {
-                                    dd("acá vamos");
-                                    if ($fechaInicioContrato <= $arLicencia->getFechaDesde()) {
+                                    if ($arContrato->getFechaDesde() <= $arLicencia->getFechaDesde()) {
+                                        $intDias = $arLicencia->getFechaDesde()->diff($arLicencia->getFechaHasta());
+                                        $intDias = $intDias->format('%a');
+                                        $intDias = $intDias + 1;
+                                        $intDiasCobro = $intDias;
+//                                        $douPorcentajePago = $arLicencia->getLicenciaTipoRel()->getPagoConceptoRel()->getPorPorcentaje();
+//                                        $arLicencia->setPorcentajePago($douPorcentajePago);
 
-                                    }else{
-
+                                        $arLicencia->setFecha(new \DateTime('now'));
+                                        $arLicencia->setEmpleadoRel($arEmpleado);
+                                        $arLicencia->setGrupoRel($arContrato->getGrupoRel());
+                                        $arLicencia->setEntidadSaludRel($arContrato->getEntidadSaludRel());
+                                        $arLicencia->setCodigoUsuario($this->getUser()->getUserName());
+                                        $arLicencia->setDiasCobro($intDiasCobro);
+                                        $arLicencia->setCantidad($intDias);
+                                        $arLicencia->setMaternidad($arLicencia->getLicenciaTipoRel()->getMaternidad());
+                                        $arLicencia->setPaternidad($arLicencia->getLicenciaTipoRel()->getPaternidad());
+                                        $floVrLicencia = 0;
+                                        $douVrDia = $arContrato->getVrSalario() / 30;
+                                        $salarioEmpleado = $douVrDia * 30;
+                                        $douVrDiaSalarioMinimo = $arContrato->getVrSalario() / 30;
+//                                        $arLicencia->setPorcentajePago($douPorcentajePago);
+                                        if ($arLicencia->getVrIbcPropuesto() > 0) {
+                                            $arrIbc = array('respuesta' => true, 'ibc' => $arLicencia->getVrIbcPropuesto(), 'dias' => 30);
+                                        } else {
+                                            $arrIbc = array('respuesta' => true, 'ibc' => $arContrato->getVrSalario(), 'dias' => 30);
+                                        }
+                                        if ($arLicencia->getLicenciaTipoRel()->getMaternidad() ||
+                                            $arLicencia->getLicenciaTipoRel()->getPaternidad() ||
+                                            $arLicencia->getLicenciaTipoRel()->getRemunerada()) {
+                                            //$arConfiguracion->getLiquidarLicenciasIbcMesAnterior() &&
+                                            if ((($arLicencia->getLicenciaTipoRel()->getMaternidad() || $arLicencia->getLicenciaTipoRel()->getPaternidad()))) {
+                                                $arrIbc = $em->getRepository(RhuAporteDetalle::class
+                                                )->ibcMesAnterior($arLicencia->getFechaDesde()->format('Y'), $arLicencia->getFechaDesde()->format('m'), $arEmpleado->getCodigoEmpleadoPk());
+                                                if ($arLicencia->getVrIbcPropuesto() > 0) {
+                                                    $arrIbc = array('respuesta' => true, 'ibc' => $arLicencia->getVrIbcPropuesto(), 'dias' => 30);
+                                                }
+                                                if (!$arrIbc['respuesta']) {
+                                                    $arLicenciaAnterior = $em->getRepository( RhuLicencia::class)->findOneBy(array('codigoEmpleadoFk' => $arEmpleado->getCodigoEmpleadoPk(), 'codigoLicenciaTipoFk' => $arLicencia->getLicenciaTipoRel()->getCodigoLicenciaTipoPk()));
+                                                    if ($arLicenciaAnterior) {
+                                                        if (($arLicenciaAnterior->getDiasCobro() <= 31 && $arLicencia->getLicenciaTipoRel()->getMaternidad() == $arLicenciaAnterior->getMaternidad()) || ($arLicenciaAnterior->getDiasCobro() <= 8 && $arLicencia->getLicenciaTipoRel()->getPaternidad == $arLicenciaAnterior->getPaternidad()))
+                                                            $arrIbc = array('respuesta' => true, 'ibc' => $arLicenciaAnterior->getVrIbcMesAnterior(), 'dias' => $arLicenciaAnterior->getDiasIbcMesAnterior());
+                                                    }
+                                                }
+                                                $arLicencia->setVrIbcMesAnterior($arrIbc['ibc']);
+                                                $arLicencia->setDiasIbcMesAnterior($arrIbc['dias']);
+                                                if ($arrIbc['ibc'] != 0 && $arrIbc['dias'] != 0) {
+                                                    $douVrDia = $arrIbc['ibc'] / $arrIbc['dias'];
+                                                }
+                                                $salarioEmpleado = $douVrDia * 30;
+                                            }
+                                            if ($arContrato->getLiquidarLicenciasIbcMesAnterior() && (($arLicencia->getLicenciaTipoRel()->getRemunerada()))) {
+                                                $arrIbc = $em->getRepository(RhuAporteDetalle::class)->ibcMesAnterior($arLicencia->getFechaDesde()->format('Y'), $arLicencia->getFechaDesde()->format('m'), $arEmpleado->getCodigoEmpleadoPk());
+                                                $arLicencia->setVrIbcMesAnterior($arrIbc['ibc']);
+                                                $arLicencia->setDiasIbcMesAnterior($arrIbc['dias']);
+                                            }
+                                        }
+                                        if ($salarioEmpleado <= $arContrato->getVrSalario()) {
+                                            $floVrLicencia = $intDiasCobro * $douVrDiaSalarioMinimo;
+                                        } else {
+                                            if ($arEmpleado->getVrSalario()) {
+                                                $floVrLicencia = $intDiasCobro * $douVrDia;
+                                            }
+                                        }
+                                        $arLicencia->setVrCobro(round($floVrLicencia));
+                                        if ($arLicencia->getVrPropuesto() > 0) {
+                                            $floVrLicencia = $arLicencia->getVrPropuesto();
+                                        }
+                                        $arLicencia->setVrCobro(0);
+                                        $arLicencia->setVrLicencia(round($floVrLicencia));
+                                        $arLicencia->setVrSaldo(round($floVrLicencia));
+//                                        if ($arEmpleado->getCodigoContratoActivoFk() != '') {
+//                                            $arContrato = $em->getRepository(RhuContrato::class)->find($arEmpleado->getCodigoContratoActivoFk());
+//                                        } else {
+//                                            $arContrato = $em->getRepository(RhuContrato::class)->find($arEmpleado->getCodigoContratoUltimoFk());
+//                                        }
+                                        $arLicencia->setContratoRel($arContrato);
+                                        $em->persist($arLicencia);
+                                        $em->flush();
+                                        return $this->redirect($this->generateUrl('recursohumano_movimiento_nomina_licencia_detalle', array("id"=>$arLicencia->getCodigoLicenciaPk())));
                                     }
-                                else{
-                                    Mensaje::error("Existe otra licencia en este rango de fechas");
+                                } else {
+                                    Mensaje::error("La fecha de inicio del contrato es mayor a la licencia");
                                 }
-                            }else{
-                                Mensajes::error("Hay incapacidades que se cruzan con la fecha de la licencia");
-
+                            } else {
+                                Mensaje::error("Existe otra licencia en este rango de fechas");
                             }
-                        }else{
-                            Mensajes::error("La fecha desde debe ser inferior o igual a la fecha hasta");
+                        } else {
+                            Mensajes::error("Hay incapacidades que se cruzan con la fecha de la licencia");
                         }
-//                        $arLicencia->setFecha(new \DateTime('now'));
-//                        $arLicencia->setEmpleadoRel($arEmpleado);
-//                        $arLicencia->setGrupoRel($arContrato->getGrupoRel());
-//                        $arLicencia->setEntidadSaludRel($arContrato->getEntidadSaludRel());
-//                        $arLicencia->setCodigoUsuario($this->getUser()->getUserName());
-//                        $em->persist($arLicencia);
-//                        $em->flush();
-                        return $this->redirect($this->generateUrl('recursohumano_movimiento_nomina_licencia_lista', ['id' => $arLicencia->getCodigoLicenciaPk()]));
-                    }else{
-                        Mensajes::error("El contrato no existe");
+                    } else {
+                        Mensajes::error("La fecha desde debe ser inferior o igual a la fecha hasta");
                     }
-
-                }else{
-                    Mensajes::error("El empleado no existe");
+                    return $this->redirect($this->generateUrl('recursohumano_movimiento_nomina_licencia_lista', ['id' => $arLicencia->getCodigoLicenciaPk()]));
+                } else {
+                    Mensajes::error("El contrato no existe");
                 }
 
+            } else {
+                Mensajes::error("El empleado no existe");
             }
+
         }
         return $this->render('recursohumano/movimiento/nomina/licencia/nuevo.html.twig', [
             'form' => $form->createView(),
             'arIncapacidad' => $arLicencia,
         ]);
     }
+
+
+
+
 
     /**
      * @Route("recursohumano/moviento/nomina/licencia/detalle/{id}", name="recursohumano_movimiento_nomina_licencia_detalle")
--- src/Form/Type/RecursoHumano/LicenciaTipoType.php
+++ src/Form/Type/RecursoHumano/LicenciaTipoType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -26,14 +27,31 @@ class LicenciaTipoType extends AbstractType
         ]);
     }
 
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre', TextType::class, ['required' => false,'label' => 'Nombre:'])
+            ->add('afectaSalud', CheckboxType::class, array('required'  => false))
+            ->add('ausentismo', CheckboxType::class, array('required'  => false))
+            ->add('maternidad', CheckboxType::class, array('required'  => false))
+            ->add('paternidad', CheckboxType::class, array('required'  => false))
+            ->add('remunerada', CheckboxType::class, array('required'  => false))
+            ->add('suspensionContratoTrabajo', CheckboxType::class, array('required'  => false))
+            ->add('guardar', SubmitType::class, ['label' => 'Guardar','attr' => ['class' => 'btn btn-sm btn-primary']]);
+        ;
+    }
+
     public function getEstructuraPropiedadesLista()
     {
         $campos = '[
-            {"campo":"codigoLicenciaTipoPk", "tipo":"pk",    "ayuda":"Codigo del registro", "titulo":"ID"},
             {"campo":"nombre",              "tipo":"texto", "ayuda":"Nombre del registro", "titulo":"NOMBRE"},
-            {"campo":"subTipo",             "tipo":"texto", "ayuda":"Subtipo del registro L = LICENCIA, I = INCAPACIDAD", "titulo":"SUBTIPO"},
-            {"campo":"codigoConceptoFk",    "tipo":"texto", "ayuda":"Codigo concepto ",    "titulo":"CONCEPTO"},
-            {"campo":"abreviatura",         "tipo":"texto", "ayuda":"Abreviatura del nombre del registro ",    "titulo":"ABR"}
+            {"campo":"afectaSalud",    "tipo":"bool",  "ayuda":"Afecta salud",    "titulo":"SALUD"},
+            {"campo":"ausentismo",    "tipo":"bool",  "ayuda":"Ausentismo",    "titulo":"AUSENTISMO"},
+            {"campo":"maternidad",    "tipo":"bool",  "ayuda":"Maternidad",    "titulo":"MATERNIDAD"},
+            {"campo":"paternidad",    "tipo":"bool",  "ayuda":"Paternidad",    "titulo":"PATERNIDAD"},
+            {"campo":"remunerada",    "tipo":"bool",  "ayuda":"Remunerada",    "titulo":"RENUN"},
+            {"campo":"suspensionContratoTrabajo",    "tipo":"bool",  "ayuda":"Suspension Contrato trabajo",    "titulo":"SCT"}
+
         ]';
         return $campos;
     }

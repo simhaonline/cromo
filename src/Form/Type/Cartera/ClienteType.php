021a6d78b9d7ac03062a0c6fc005854510a6d4d1
andres <afcanop@gmail.com>
2019-10-01 08:36:01 -0500

    fix: label de formulario de cliente cartera

--- src/Form/Type/Cartera/ClienteType.php
+++ src/Form/Type/Cartera/ClienteType.php
@@ -27,6 +27,7 @@ class ClienteType extends AbstractType {
                         ->orderBy('fp.nombre', 'ASC');
                 },
                 'choice_label' => 'nombre',
+                'label'=>'Forma pago'
             ))
             ->add('ciudadRel', EntityType::class, array(
                 'class' => GenCiudad::class,
@@ -35,20 +36,20 @@ class ClienteType extends AbstractType {
                         ->orderBy('c.nombre', 'ASC');
                 },
                 'choice_label' => 'nombre',
+                'label'=>'Ciudad'
             ))
-            ->add('numeroIdentificacion', IntegerType::class, array('required' => true))
-            ->add('digitoVerificacion',IntegerType::class, array('required' => false))
-            ->add('nombreCorto',TextType::class, array('required' => true))
-            ->add('direccion',TextType::class, array('required' => false))
-            ->add('telefono',IntegerType::class, array('required' => false))
-            ->add('celular',IntegerType::class, array('required' => false))
-            ->add('fax', IntegerType::class, array('required' => false))
-            ->add('correo', TextType::class, array('required' => false))
-            ->add('contacto', TextType::class, array('required' => false))
-            ->add('plazoPago', IntegerType::class, array('required' => true))
-            ->add('contactoTelefono', IntegerType::class, array('required' => false))
-            ->add('guardar', SubmitType::class,array('label'=>'Guardar'))
-            ->add('guardarnuevo', SubmitType::class,array('label'=>'Guardar y nuevo'));
+            ->add('numeroIdentificacion', IntegerType::class, array('required' => true, 'label'=>'Numero identificación'))
+            ->add('digitoVerificacion',IntegerType::class, array('required' => false, 'label'=>'Digito verificación' ))
+            ->add('nombreCorto',TextType::class, array('required' => true, 'label'=>'Nombre'))
+            ->add('direccion',TextType::class, array('required' => false, 'label'=>'Dirección'))
+            ->add('telefono',IntegerType::class, array('required' => false, 'label'=>'Teléfono'))
+            ->add('celular',IntegerType::class, array('required' => false, 'label'=>'Celular'))
+            ->add('fax', IntegerType::class, array('required' => false, 'label'=>'fax'))
+            ->add('correo', TextType::class, array('required' => false, 'label'=>'Correo'))
+            ->add('contacto', TextType::class, array('required' => false, 'label'=>'contacto'))
+            ->add('plazoPago', IntegerType::class, array('required' => true,'label'=>'Plazo pago'))
+            ->add('contactoTelefono', IntegerType::class, array('required' => false,'label'=>'Contacto teléfono'))
+            ->add('guardar', SubmitType::class,array('label'=>'Guardar'));
     }
 
     /**

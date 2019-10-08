012bd5973c20b86b02a3dc8723393a21671ad048
andres <afcanop@gmail.com>
2019-10-08 10:40:51 -0500

    fix: cambio de Controller a AbstractController en inventario inicioController

--- src/Controller/Inventario/General/InicioController.php
+++ src/Controller/Inventario/General/InicioController.php
@@ -2,18 +2,17 @@
 
 namespace App\Controller\Inventario\General;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class InicioController extends Controller
+class InicioController extends AbstractController
 {
     /**
      * @Route("/inventario/general/inicio", name="inventario_general_general_inicio_ver")
      */
     public function lista()
     {
-
-
         return $this->render('inventario/general/inicio.html.twig'
         );
     }

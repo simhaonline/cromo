3b898c29ad32ea6993c02337bb75c7c66add786f
andres <afcanop@gmail.com>
2019-10-29 11:51:42 -0500

    feat:  archivo plano de bbva

--- src/Controller/Tesoreria/General/ArchivoPlanoController.php
+++ src/Controller/Tesoreria/General/ArchivoPlanoController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Tesoreria\General;
 
 
 use App\Entity\General\GenConfiguracion;
+use App\Entity\RecursoHumano\RhuConfiguracion;
 use App\Entity\Tesoreria\TesMovimiento;
 use App\Entity\Tesoreria\TesMovimientoDetalle;
 use App\Utilidades\Mensajes;
@@ -47,12 +48,14 @@ class ArchivoPlanoController extends AbstractController
                     'fechaAplicacion' => $form->get('fechaAplicacion')->getData() ? $form->get('fechaAplicacion')->getData()->format('ymd') : null,
                 ];
                 switch ($banco){
-                    case 'BBVA':
+                    case 'bbva':
+                        $this->generarArchivoBBVA($arMovimiento);
                         break;
                     case 'BancolombiaSap':
                         $this->generarArchivoBancolombiaSap($arMovimiento, $rawDataForm);
                         break;
                     case 'BancolombiaPab':
+                        $this->generarArchivoBancolombiaPab($arMovimiento, $rawDataForm);
                         break;
                     default:
                        Mensajes::error("seleccionar banco");
@@ -62,6 +65,138 @@ class ArchivoPlanoController extends AbstractController
         return $this->render('tesoreria/archivoPlano/archivoPlano.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @param $arMovimiento TesMovimiento
+     * @throws \Exception
+     */
+    private function generarArchivoBBVA($arMovimiento)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $arConfiguracionGeneral = $em->getRepository(GenConfiguracion::class)->find(1);
+        $arRhuConfiguracion = $em->getRepository(RhuConfiguracion::class)->find(1);
+        $dateNow = new \DateTime('now');
+        $dateNow = $dateNow->format('YmdHis');
+        $strNombreArchivo = "pagoBBVA{$arMovimiento->getNumero()}_{$dateNow}.txt";
+        $strArchivo = $arConfiguracionGeneral->getRutaTemporal() . $strNombreArchivo;
+        ob_clean();
+        $archivo = fopen($strArchivo, "a") or die("Problemas en la creacion del archivo plano");
+        $strValorTotal = 0;
+        $arMovimientoDetalles = $em->getRepository(TesMovimientoDetalle::class)->findBy(array('codigoMovimientoFk' => $arMovimiento->getCodigoMovimientoPk()));
+        foreach ($arMovimientoDetalles AS $arMovimientoDetalle) {
+            $strValorTotal += round($arMovimientoDetalle->getVrPago());
+        }
+        //Inicio cuerpo
+        foreach ($arMovimientoDetalles AS $arMovimientoDetalle) {
+            if ($arMovimientoDetalle->getVrPago() > 0) {
+                $varTipoDocumento = $arMovimientoDetalle->getCuentaPagarRel()->getTerceroRel()->getCodigoIdentificacionFk();
+                switch ($varTipoDocumento) {
+                    //'01' - Cédula de ciudadanía
+                    case 'CC':
+                        $strTipoDocumento = '01';
+                        break;
+                    //'02' - Cédula de extranjería
+                    case 'CE':
+                        $strTipoDocumento = '02';
+                        break;
+                    //'03' - N.I.T.
+                    case 'NI':
+                        $strTipoDocumento = '03';
+                        break;
+                    //'04' - Tarjeta de Identidad
+                    case 'TI':
+                        $strTipoDocumento = '04';
+                        break;
+                    //'05' - Pasaporte
+                    case 'PA':
+                        $strTipoDocumento = '05';
+                        break;
+                    // '06' - Sociedad extranjera sin N.I.T. En Colombia
+                    case 'TDE':
+                        $strTipoDocumento = '06';
+                        break;
+                }
+
+                //Tipo de identificacion del empleado
+                fputs($archivo, $strTipoDocumento);
+
+                //Numero de identificacion del empleado
+                fputs($archivo, $this->RellenarNr($arMovimientoDetalle->getCuentaPagarRel()->getTerceroRel()->getNumeroIdentificacion(), "0", 15));
+
+                fputs($archivo, '01');
+
+                //Codigo general del banco o codigo interface
+                fputs($archivo, $this->RellenarNr($arMovimientoDetalle->getTerceroRel()->getBancoRel()->getCodigoInterface(), "0", 4));
+
+                //Numero de cuenta del empleado y se valida si al cuenta es de BBVA o pertenece a un banco diferente
+                if ($arMovimientoDetalle->getTerceroRel()->getBancoRel()->getCodigoInterface() != 13) {
+                    fputs($archivo, '0000000000000000');
+                    switch ($arMovimiento->getTerceroRel()->getCodigoCuentaTipoFk()) {
+                        case 'S':
+                            $tipoCuenta = '02';
+                            break;
+                        case 'D':
+                            $tipoCuenta = '01';
+                            break;
+                    }
+//                    fputs($archivo, $this->RellenarNr2($tipoCuenta . $arEgresoDetalle->getCuentaPagarRel()->getTerceroRel()->getCuenta(), ' ', 19, 'D'));
+                    fputs($archivo, $this->RellenarNr2($tipoCuenta . $arMovimientoDetalle->getCuentaPagarRel()->getCuenta(), ' ', 19, 'D'));
+                } else {
+                    if ($arRhuConfiguracion->getConcatenarOfinaCuentaBbva()) {
+                        $oficina = substr($arMovimientoDetalle->getCuentaPagarRel()->getCuenta(), 0, 3);
+                        $cuenta = substr($arMovimientoDetalle->getCuentaPagarRel()->getCuenta(), 3);
+                        $strRellenar = "";
+                        if ($arMovimiento->getTerceroRel()->getCodigoCuentaTipoFk() == "S") {
+                            $strRellenar = '000200';
+                        } elseif ($arMovimiento->getTerceroRel()->getCodigoCuentaTipoFk() == "D") {
+                            $strRellenar = '000100';
+                        }
+                        fputs($archivo, $this->RellenarNr2('0' . $oficina . '' . $strRellenar . '' . $cuenta, ' ', 16, 'D'));
+                    } else {
+                        fputs($archivo, $this->RellenarNr2($arMovimientoDetalle->getCuentaPagarRel()->getTerceroRel()->getCuenta(), ' ', 16, 'D'));
+                    }
+                    fputs($archivo, '0000000000000000000');
+                }
+
+                //Valor entero del pago
+                fputs($archivo, $this->RellenarNr($arMovimientoDetalle->getVrPago(), '0', 13));
+
+                //Valor decimal del pago
+                fputs($archivo, $this->RellenarNr('0', '0', 2));
+
+                //Fecha limite de pago, no aplica
+                fputs($archivo, '000000000000');
+
+                //Nombre del empleado
+                fputs($archivo, $this->RellenarNr2(substr(utf8_decode($arMovimientoDetalle->getCuentaPagarRel()->getTerceroRel()->getNombreCorto()), 0, 36), " ", 36, 'D'));
+
+                //Direccion del empleado
+                fputs($archivo, $this->RellenarNr2('MEDELLIN', " ", 36, 'D'));
+
+                //2da direccion del empleado
+                fputs($archivo, $this->RellenarNr2(" ", " ", 36, 'D'));
+
+                //Email del empleado
+                fputs($archivo, $this->RellenarNr2("", " ", 48, 'D'));
+
+                //Concepto del pago
+                fputs($archivo, $this->RellenarNr2("NOMINA", " ", 40, 'D'));
+                fputs($archivo, "\n");
+            }
+        }
+        fclose($archivo);
+        $em->flush();
+        //Fin cuerpo
+        header('Content-Description: File Transfer');
+        header('Content-Type: text/csv; charset=ISO-8859-15');
+        header('Content-Disposition: attachment; filename=' . basename($strArchivo));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($strArchivo));
+        readfile($strArchivo);
+        exit;
     }
 
     /**
@@ -117,60 +252,60 @@ class ArchivoPlanoController extends AbstractController
         exit;
     }
 
-    private function generarArchivoBancolombiaPab($arPagoBanco)
+    /**
+     * @param $arMovimiento TesMovimiento
+     */
+    private function generarArchivoBancolombiaPab($arMovimiento, $rawDataForm)
     {
-        $em = $this->getDoctrine()->getManager();
-        $arConfiguracionGeneral = $em->getRepository(GenConfiguracion::class)->find(1);
-        $strNombreArchivo = "pagoPab" . date('YmdHis') . ".txt";
-        $auxZero = "000000";
-        $strArchivo = $arConfiguracionGeneral->getRutaTemporal() . $strNombreArchivo;
-        ob_clean();
-        $ar = fopen($strArchivo, "a") or die("Problemas en la creacion del archivo plano");
-        $strValorTotal = 0;
-        $arPagosBancoDetalle = $em->getRepository( TesMovimientoDetalle::class)->findBy(array('codigoBancoFk' => $arPagoBanco->getCodigoPagoBancoPk()));
-        foreach ($arPagosBancoDetalle AS $arPagoBancoDetalle) {
-            $strValorTotal += round($arPagoBancoDetalle->getVrPago());
-        }
-        // Encabezado
-        $strNitEmpresa = $this->RellenarNr(utf8_decode($arConfiguracionGeneral->getNitEmpresa()), "0", 15);
-        $strNombreEmpresa = $this->RellenarNr(utf8_decode(substr($arConfiguracionGeneral->getNombreEmpresa(), 0, 16)), 0, 16);
-        $strTipoPagoSecuencia = "225          ";
-        $strSecuencia = $arPagoBanco->getSecuencia() . "1";
-        $strFechaCreacion = $arPagoBanco->getFechaTrasmision()->format('Ymd');
-        $strFechaAplicacion = $arPagoBanco->getFechaAplicacion()->format('Ymd');
-        $strNumeroRegistros = $this->RellenarNr($arPagoBanco->getNumeroRegistros(), "0", 6);
-        $strValorTotal = "00000000000000000" . $this->RellenarNr($strValorTotal, "0", 15) . "00";
-        //Fin encabezado
-        //(1) Tipo de registro, (10) Nit empresa, (225PAGO NOMI) descripcion transacion, (yymmdd) fecha creacion, (yymmdd) fecha aplicacion, (6) Numero de registros, (17) sumatoria de creditos, (11) Cuenta cliente a debitar, (1) Tipo de cuenta a debitar
-        fputs($ar, "1" . $strNitEmpresa . "I" . "               " . $strTipoPagoSecuencia . $strFechaCreacion . $strSecuencia . $strFechaAplicacion . $strNumeroRegistros . $strValorTotal . $this->RellenarNr($arPagoBanco->getCuentaRel()->getCuenta(), 0, 11) . $this->RellenarNr2($arPagoBanco->getCuentaRel()->getTipo(), ' ', 150, 'D') . "\n");
-        //Inicio cuerpo
-        foreach ($arPagosBancoDetalle AS $arPagoBancoDetalle) {
-            fputs($ar, "6"); //(1)Tipo registro
-            fputs($ar, $this->RellenarNr2($arPagoBancoDetalle->getEmpleadoRel()->getNumeroIdentificacion(), ' ', 15, 'D')); //(15) Nit del beneficiario
-            fputs($ar, $this->RellenarNr2(substr($arPagoBancoDetalle->getEmpleadoRel()->getNombre1() . ' ' . $arPagoBancoDetalle->getEmpleadoRel()->getNombre2(), 0, 30), " ", 30, 'D')); // (30) Nombre del beneficiario
-            fputs($ar, $arPagoBancoDetalle->getEmpleadoRel()->getBancoRel()->getCodigoGeneralBancolombia() != "" ? $this->RellenarNr($arPagoBancoDetalle->getEmpleadoRel()->getBancoRel()->getCodigoGeneralBancolombia(), 0, 9) : "005600078"); // (9) Banco cuenta del beneficiario
-            fputs($ar, $arPagoBancoDetalle->getCuenta()); // (17) Nro cuenta beneficiario
-            fputs($ar, "      "); // (17) Nro cuenta beneficiario
-            fputs($ar, "337"); // (1) Indicador de lugar de pago (2) y tipo de transacción (37)
-            $duoValorNetoPagar = round($arPagoBancoDetalle->getVrPago()); // (17) Valor transacción
-            fputs($ar, $this->RellenarNr($duoValorNetoPagar, "0", 15) . "00");
-            fputs($ar, $this->RellenarNr2($strFechaAplicacion, ' ', 28, 'D'));
+//        $em = $this->getDoctrine()->getManager();
+//        $arConfiguracionGeneral = $em->getRepository(GenConfiguracion::class)->find(1);
+//        $strNombreArchivo = "pagoPab" . date('YmdHis') . ".txt";
+//        $auxZero = "000000";
+//        $strArchivo = $arConfiguracionGeneral->getRutaTemporal() . $strNombreArchivo;
+//        ob_clean();
+//        $ar = fopen($strArchivo, "a") or die("Problemas en la creacion del archivo plano");
+//        $strValorTotal = $arMovimiento->getVrTotalNeto();
+//        $arMovimientoDetalles = $em->getRepository(TesMovimientoDetalle::class)->findBy(['codigoMovimientoFk'=>$arMovimiento->getCodigoMovimientoPk()]);
+//        // Encabezado
+//        $strNitEmpresa = $this->RellenarNr(utf8_decode($arConfiguracionGeneral->getNit()), "0", 15);
+//        $strNombreEmpresa = $this->RellenarNr(utf8_decode(substr($arConfiguracionGeneral->getNombre(), 0, 16)), 0, 16);
+//        $strTipoPagoSecuencia = "225          ";
+//        $strSecuencia = $rawDataForm['secuencia']. "1";
+//        $strFechaCreacion =  $rawDataForm['fechaTrasmision'] ;
+//        $strFechaAplicacion = $rawDataForm['fechaAplicacion'];
+//        $strNumeroRegistros = $this->RellenarNr(count($arMovimientoDetalles), "0", 6);
+//        $strValorTotal = "00000000000000000" . $this->RellenarNr($strValorTotal, "0", 15) . "00";
+//        //Fin encabezado
+//        //(1) Tipo de registro, (10) Nit empresa, (225PAGO NOMI) descripcion transacion, (yymmdd) fecha creacion, (yymmdd) fecha aplicacion, (6) Numero de registros, (17) sumatoria de creditos, (11) Cuenta cliente a debitar, (1) Tipo de cuenta a debitar
+//        fputs($ar, "1" . $strNitEmpresa . "I" . "               " . $strTipoPagoSecuencia . $strFechaCreacion . $strSecuencia . $strFechaAplicacion . $strNumeroRegistros . $strValorTotal . $this->RellenarNr($arMovimientoDetalle->getCuenta(), 0, 11) . $this->RellenarNr2($arPagoBanco->getCuentaRel()->getTipo(), ' ', 150, 'D') . "\n");
+//        //Inicio cuerpo
+//        foreach ($arMovimientoDetalles AS $arMovimientoDetalle) {
+//            fputs($ar, "6"); //(1)Tipo registro
+//            fputs($ar, $this->RellenarNr($arMovimientoDetalle->getTerceroRel()->getNumeroIdentificacion(), "0", 15)); //(15) Nit del beneficiario
+//            fputs($ar, $this->RellenarNr2(substr($arMovimientoDetalle->getTerceroRel()->getNombre1() . ' ' . $arMovimientoDetalle->getTerceroRel()->getNombre2(), 0, 30), " ", 30, 'D')); // (30) Nombre del beneficiario
+//            fputs($ar, $arMovimientoDetalle->getTerceroRel()->getBancoRel()->getCodigoBancolombia() != "" ? $this->RellenarNr($arMovimientoDetalle->getTerceroRel()->getBancoRel()->getCodigoBancolombia(), "0", 9) : "005600078"); // (9) Banco cuenta del beneficiario
+//            fputs($ar, $this->RellenarNr($arMovimientoDetalle->getCuenta(), "0", 17)); // (17) Nro cuenta beneficiario
+//            fputs($ar, "      "); // (17) Nro cuenta beneficiario
+//            fputs($ar, "337"); // (1) Indicador de lugar de pago (2) y tipo de transacción (37)
+//            $duoValorNetoPagar = round($arMovimientoDetalle->getVrPago()); // (17) Valor transacción
+//            fputs($ar, $this->RellenarNr($duoValorNetoPagar, "0", 15) . "00");
+//            fputs($ar, $this->RellenarNr2($strFechaAplicacion, ' ', 28, 'D'));
+////            fputs($ar, "\n");
+//            fputs($ar, $this->RellenarNr2($auxZero, ' ', 143, 'D'));
 //            fputs($ar, "\n");
-            fputs($ar, $this->RellenarNr2($auxZero, ' ', 143, 'D'));
-            fputs($ar, "\n");
-        }
-        fclose($ar);
-        $em->flush();
-        //Fin cuerpo
-        header('Content-Description: File Transfer');
-        header('Content-Type: text/csv; charset=ISO-8859-15');
-        header('Content-Disposition: attachment; filename=' . basename($strArchivo));
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($strArchivo));
-        readfile($strArchivo);
-        exit;
+//        }
+//        fclose($ar);
+//        $em->flush();
+//        //Fin cuerpo
+//        header('Content-Description: File Transfer');
+//        header('Content-Type: text/csv; charset=ISO-8859-15');
+//        header('Content-Disposition: attachment; filename=' . basename($strArchivo));
+//        header('Expires: 0');
+//        header('Cache-Control: must-revalidate');
+//        header('Pragma: public');
+//        header('Content-Length: ' . filesize($strArchivo));
+//        readfile($strArchivo);
+//        exit;
     }
 
     //Rellenar numeros

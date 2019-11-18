eba761efcc4512af511794a0c6fa98f7fe8c6391
desarrollo <maestradaz3@gmail.com>
2019-11-18 16:19:39 -0500

    cambio en mostrar contraseña en gen usurio

--- src/Controller/General/Seguridad/SeguridadController.php
+++ src/Controller/General/Seguridad/SeguridadController.php
@@ -30,7 +30,7 @@ class SeguridadController extends AbstractController
     /**
      * @Route("/general/seguridad/usuario/lista", name="general_seguridad_usuario_lista")
      */
-    public function lista(Request $request, PaginatorInterface $paginator )
+    public function lista(Request $request, PaginatorInterface $paginator)
     {
         $em = $this->getDoctrine()->getManager();
         $form = $this->createFormBuilder()
@@ -46,19 +46,18 @@ class SeguridadController extends AbstractController
                 $this->generarExcel();
             }
             if ($form->get('btnExcelPermiso')->isClicked()) {
-                $arUsuariosPermisos=$em->getRepository('App:Seguridad\Usuario')->exportarExcelPermisos();
-                $permisos=array('LISTA','NUEVO','DETALLE','AUTORIZAR','APROBAR','ANULAR');
-                for($i=0; $i<count($arUsuariosPermisos);$i++){
-                    foreach ($permisos as $permiso){
-                        if(!$arUsuariosPermisos[$i][$permiso]){
-                            $arUsuariosPermisos[$i][$permiso]="NO";
-                        }
-                        else{
-                            $arUsuariosPermisos[$i][$permiso]="SI";
+                $arUsuariosPermisos = $em->getRepository('App:Seguridad\Usuario')->exportarExcelPermisos();
+                $permisos = array('LISTA', 'NUEVO', 'DETALLE', 'AUTORIZAR', 'APROBAR', 'ANULAR');
+                for ($i = 0; $i < count($arUsuariosPermisos); $i++) {
+                    foreach ($permisos as $permiso) {
+                        if (!$arUsuariosPermisos[$i][$permiso]) {
+                            $arUsuariosPermisos[$i][$permiso] = "NO";
+                        } else {
+                            $arUsuariosPermisos[$i][$permiso] = "SI";
                         }
                     }
                 }
-                General::get()->setExportar($arUsuariosPermisos,'ExcelPermiso');
+                General::get()->setExportar($arUsuariosPermisos, 'ExcelPermiso');
             }
         }
         $arUsuarios = $paginator->paginate($em->getRepository('App:Seguridad\Usuario')->findAll(), $request->query->getInt('page', 1), 30);
@@ -78,12 +77,15 @@ class SeguridadController extends AbstractController
         $respuesta = '';
         $arrPropiedadesClaves = ['required' => true];
         $id = $this->verificarUsuario($hash);
+        $editarContrasenas = true;
+
         if ($id != "") {
             $arUsuario = $em->getRepository('App:Seguridad\Usuario')->find($id);
             if (!$arUsuario) {
                 return $this->redirect($this->generateUrl('general_seguridad_usuario_lista'));
             } else {
                 $arrPropiedadesClaves = ['attr' => ['readonly' => 'readonly']];
+                $editarContrasenas = false;
             }
         }
         $arrPropiedadesOperacionRel = [
@@ -112,35 +114,37 @@ class SeguridadController extends AbstractController
             'placeholder' => "",
             'data' => ""
         ];
-        if($arUsuario->getOperacionRel()){
+        if ($arUsuario->getOperacionRel()) {
             $arrPropiedadesOperacionRel["data"] = $em->getReference(TteOperacion::class, $arUsuario->getCodigoOperacionFk());
         }
-        if($arUsuario->getAsesorRel()){
+        if ($arUsuario->getAsesorRel()) {
             $arrPropiedadesAsesorRel["data"] = $em->getReference(Usuario::class, $arUsuario->getCodigoAsesorFk());
         }
         //se crea el formulario acá por que el campo rol genera error con el core de symfony
         $form = $this->createFormBuilder()
-            ->add('operacionRel',EntityType::class, $arrPropiedadesOperacionRel)
-            ->add('asesorRel',EntityType::class, $arrPropiedadesAsesorRel)
-            ->add('txtUser', TextType::class, ['data' => $arUsuario->getUsername(), 'required'=>true, 'constraints'=>array(
-                new NotBlank(array("message"=>"El nombre de usuario es obligatorio")),
-                new Regex(array('pattern'=>"/[A-Za-z0-9]/",'message'=>"El nombre de usuario no puedo contener caracteres")),
+            ->add('operacionRel', EntityType::class, $arrPropiedadesOperacionRel)
+            ->add('asesorRel', EntityType::class, $arrPropiedadesAsesorRel)
+            ->add('txtUser', TextType::class, ['data' => $arUsuario->getUsername(), 'required' => true, 'constraints' => array(
+                new NotBlank(array("message" => "El nombre de usuario es obligatorio")),
+                new Regex(array('pattern' => "/[A-Za-z0-9]/", 'message' => "El nombre de usuario no puedo contener caracteres")),
             ),
-                'disabled'=> $arUsuario->getUsername()?true:false
+                'disabled' => $arUsuario->getUsername() ? true : false
             ])
-            ->add('txtEmail', TextType::class, ['data' => $arUsuario->getEmail(),'required'=>true,'constraints'=>array(
-                new NotBlank(array("message"=>"El email es obligatorio")),
-                new Regex(array('pattern'=>"/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i",'message'=>"El email es invalido")),
+            ->add('txtEmail', TextType::class, ['data' => $arUsuario->getEmail(), 'required' => true, 'constraints' => array(
+                new NotBlank(array("message" => "El email es obligatorio")),
+                new Regex(array('pattern' => "/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i", 'message' => "El email es invalido")),
             ),
-                ])
-            ->add('txtCargo', TextType::class, ['data' => $arUsuario->getCargo(),'required' => false])
-            ->add('txtNombreCorto', TextType::class, ['data' => $arUsuario->getNombreCorto(),'required' => false])
-            ->add('txtIdentificacion', NumberType::class, ['data' => $arUsuario->getNumeroIdentificacion(),'required' => false])
-            ->add('txtTelefono', TextType::class, ['data' => $arUsuario->getTelefono(),'required' => false])
-            ->add('txtExtension', TextType::class, ['data' => $arUsuario->getExtension(),'required' => false])
-            ->add('txtClaveEscritorio', TextType::class, ['data' => $arUsuario->getClaveEscritorio(),'required' => false])
-            ->add('cboRol', ChoiceType::class, ['data' => $arUsuario->getRoles()[0],'required' => true, 'choices'=>array('Usuario'=>"ROLE_USER",'Administrador'=>"ROLE_ADMIN")])
+            ])
+            ->add('txtCargo', TextType::class, ['data' => $arUsuario->getCargo(), 'required' => false])
+            ->add('txtNombreCorto', TextType::class, ['data' => $arUsuario->getNombreCorto(), 'required' => false])
+            ->add('txtIdentificacion', NumberType::class, ['data' => $arUsuario->getNumeroIdentificacion(), 'required' => false])
+            ->add('txtTelefono', TextType::class, ['data' => $arUsuario->getTelefono(), 'required' => false])
+            ->add('txtExtension', TextType::class, ['data' => $arUsuario->getExtension(), 'required' => false])
+            ->add('txtClaveEscritorio', TextType::class, ['data' => $arUsuario->getClaveEscritorio(), 'required' => false])
+            ->add('cboRol', ChoiceType::class, ['data' => $arUsuario->getRoles()[0], 'required' => true, 'choices' => array('Usuario' => "ROLE_USER", 'Administrador' => "ROLE_ADMIN")])
             ->add('boolActivo', CheckboxType::class, ['data' => $arUsuario->getisActive(), 'label' => ' ', 'required' => false])
+            ->add('txtNuevaClave', PasswordType::class, $arrPropiedadesClaves)
+            ->add('txtConfirmacionClave', PasswordType::class, $arrPropiedadesClaves)
             ->add('btnGuardar', SubmitType::class, ['label' => 'Guardar', 'attr' => ['class' => 'btn btn-sm btn-primary']])
             ->add('restringirMovimientos', CheckboxType::class, ['data' => $arUsuario->getRestringirMovimientos(), 'label' => ' ', 'required' => false])
             ->getForm();
@@ -173,28 +177,29 @@ class SeguridadController extends AbstractController
 
                 }
 
-                    if($id === 0){
-                        $arUsuarioExistente=$em->getRepository('App:Seguridad\Usuario')->find($form->get('txtUser')->getData());
-                        if($arUsuarioExistente){
-                            Mensajes::error("Ya existe un usuario con el Nombre de usuario '{$form->get('txtUser')->getData()}'");
-                        }else{
-                            $em->persist($arUsuario);
-                            $em->flush();
-                            return $this->redirect($this->generateUrl('general_seguridad_usuario_lista'));
-                        }
-
-                    }else{
+                if ($id === 0) {
+                    $arUsuarioExistente = $em->getRepository('App:Seguridad\Usuario')->find($form->get('txtUser')->getData());
+                    if ($arUsuarioExistente) {
+                        Mensajes::error("Ya existe un usuario con el Nombre de usuario '{$form->get('txtUser')->getData()}'");
+                    } else {
                         $em->persist($arUsuario);
                         $em->flush();
-
                         return $this->redirect($this->generateUrl('general_seguridad_usuario_lista'));
                     }
+
+                } else {
+                    $em->persist($arUsuario);
+                    $em->flush();
+
+                    return $this->redirect($this->generateUrl('general_seguridad_usuario_lista'));
+                }
 
             }
         }
         return $this->render('general/seguridad/nuevo.html.twig', [
             'arUsuario' => $arUsuario,
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'editarContrasenas' =>$editarContrasenas
         ]);
     }
 
@@ -223,12 +228,12 @@ class SeguridadController extends AbstractController
             if ($form->get('btnActualizar')->isClicked()) {
                 $claveNueva = $form->get('txtNuevaClave')->getData();
                 $claveConfirmacion = $form->get('txtConfirmacionClave')->getData();
-                    if ($claveNueva == $claveConfirmacion) {
-                        $arUsuario->setPassword(password_hash($claveNueva, PASSWORD_BCRYPT));
-                        $em->persist($arUsuario);
-                    } else {
-                        $respuesta = "Las claves ingresadas no coindicen";
-                    }
+                if ($claveNueva == $claveConfirmacion) {
+                    $arUsuario->setPassword(password_hash($claveNueva, PASSWORD_BCRYPT));
+                    $em->persist($arUsuario);
+                } else {
+                    $respuesta = "Las claves ingresadas no coindicen";
+                }
             }
             if ($respuesta != '') {
                 Mensajes::error($respuesta);

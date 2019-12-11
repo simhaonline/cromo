83ae75f0e84901bacbd851b24ec4f3c8112ac669
andres <afcanop@gmail.com>
2019-12-11 13:03:39 -0500

    fix: se especifican los campan para la consulta de lista para TurTurnoRepository

--- src/Repository/Turno/TurTurnoRepository.php
+++ src/Repository/Turno/TurTurnoRepository.php
@@ -18,15 +18,31 @@ class TurTurnoRepository extends ServiceEntityRepository
     public function lista(){
         $session = new Session();
         $queryBuilder = $this->getEntityManager()->createQueryBuilder()->from(TurTurno::class, 't')
-            ->select('t');
-
+            ->select('t.codigoTurnoPk')
+            ->addSelect('t.nombre ')
+            ->addSelect('t.horas ')
+            ->addSelect('t.horaDesde ')
+            ->addSelect('t.horaHasta  ')
+            ->addSelect('t.horasDiurnas ')
+            ->addSelect('t.horasNocturnas ')
+            ->addSelect('t.novedad ')
+            ->addSelect('t.descanso ')
+            ->addSelect('t.incapacidad ')
+            ->addSelect('t.licencia ')
+            ->addSelect('t.vacacion ')
+            ->addSelect('t.ingreso ')
+            ->addSelect('t.retiro ')
+            ->addSelect('t.induccion ')
+            ->addSelect('t.ausentismo ')
+            ->addSelect('t.dia ')
+            ->addSelect('t.noche ');
         if ($session->get('filtroTurTurnoCodigoTurno') != '') {
             $queryBuilder->andWhere("t.codigoTurnoPk = '{$session->get('filtroTurTurnoCodigoTurno')}'");
         }
         if ($session->get('filtroTurTurnoNombre') != '') {
             $queryBuilder->andWhere("t.nombre LIKE '%{$session->get('filtroTurTurnoNombre')}%'");
         }
-        return $queryBuilder;
+        return $queryBuilder->getQuery()->getResult();
     }
 
 }

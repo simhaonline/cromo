790275eb6b8c7607e830df910843e0dc23bd26f9
MarioEstrada <maestradaz3@gmail.com>
2020-01-02 12:15:52 -0500

    feat: Correcciones

--- src/Controller/Turno/Informe/Juridico/contratoDetalleController.php
+++ src/Controller/Turno/Informe/Juridico/contratoDetalleController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Turno\Informe\Juridico;
 use App\Entity\Turno\TurContrato;
 use App\Entity\Turno\TurContratoDetalle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -28,6 +29,8 @@ class contratoDetalleController extends  Controller
             ->add('txtCliente', TextType::class, ['required' => false])
             ->add('fechaDesde', DateType::class, ['label' => 'Fecha desde: ',  'required' => false, 'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'data' => $session->get('filtroRhuInformeContratoDetalleFechaDesde') ? date_create($session->get('filtroRhuInformeContratoDetalleFechaDesde')): null])
             ->add('fechaHasta', DateType::class, ['label' => 'Fecha hasta: ', 'required' => false,  'widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'data' => $session->get('filtroRhuInformeContratoDetalleFechaHasta') ? date_create($session->get('filtroRhuInformeContratoDetalleFechaHasta')): null])
+            ->add('estadoAutorizado', ChoiceType::class, ['choices' => ['TODOS' => '', 'SI' => '1', 'NO' => '0'], 'required' => false])
+            ->add('estadoTerminado', ChoiceType::class, ['choices' => ['TODOS' => '', 'SIN TERMINAR' => '0', 'TERMINADO' => '1'], 'required' => false])
             ->add('btnFiltrar', SubmitType::class, ['label' => 'Filtrar', 'attr' => ['class' => 'btn btn-sm btn-default']])
             ->getForm();
         $form->handleRequest($request);
@@ -38,7 +41,7 @@ class contratoDetalleController extends  Controller
                 $session->set('filtroRhuInformeContratoDetalleFechaHasta', $form->get('fechaHasta')->getData() ? $form->get('fechaHasta')->getData()->format('Y-m-d'): null);
             }
         }
-        $arContratoDetalles = $paginator->paginate($em->getRepository(TurContratoDetalle::class)->informe(), $request->query->getInt('page', 1), 30);
+        $arContratoDetalles = $paginator->paginate($em->getRepository(TurContratoDetalle::class)->informe(), $request->query->getInt('page', 1), 500);
         return $this->render('turno/informe/juridico/contratoDetalle/lista.html.twig', [
                 'arContratoDetalles' => $arContratoDetalles,
                 'form' => $form->createView()

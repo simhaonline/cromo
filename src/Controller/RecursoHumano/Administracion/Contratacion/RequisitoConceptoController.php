7dbc5f77e6c175ebe13b21d8fd92b688670ccc7d
andres <afcanop@gmail.com>
2020-01-08 15:06:33 -0500

    Fix: controlador personalizado de REQUISITO CONCEPTO

--- src/Controller/RecursoHumano/Administracion/Contratacion/RequisitoConceptoController.php
+++ src/Controller/RecursoHumano/Administracion/Contratacion/RequisitoConceptoController.php
@@ -6,49 +6,50 @@ use App\Controller\Estructura\ControllerListenerGeneral;
 use App\Entity\RecursoHumano\RhuRequisitoConcepto;
 use App\Form\Type\RecursoHumano\RequisitoConceptoType;
 use App\General\General;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class RequisitoConceptoController extends ControllerListenerGeneral
+class RequisitoConceptoController extends AbstractController
 {
-    protected $clase = RhuRequisitoConcepto::class;
-    protected $claseFormulario = RequisitoConceptoType::class;
-    protected $claseNombre = "RhuRequisitoConcepto";
-    protected $modulo = "RecursoHumano";
-    protected $funcion = "Administracion";
-    protected $grupo = "Contratacion";
-    protected $nombre = "RequisitoConcepto";
-
     /**
-     * @param Request $request
-     * @return Response
-     * @throws \PhpOffice\PhpSpreadsheet\Exception
-     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
      * @Route("/recursohumano/administracion/contratacion/requisitoconcepto/lista", name="recursohumano_administracion_contratacion_requisitoconcepto_lista")
      */
-    public function lista(Request $request)
+    public function lista(Request $request, PaginatorInterface $paginator )
     {
         $this->request = $request;
         $em = $this->getDoctrine()->getManager();
-        $paginator = $this->get('knp_paginator');
         $form = $this->createFormBuilder()
-            ->add('btnEliminar', SubmitType::class, ['label' => 'Eliminar', 'attr' => ['class' => 'btn-sm btn btn-danger']])
-            ->add('btnExcel', SubmitType::class, ['label' => 'Excel', 'attr' => ['class' => 'btn-sm btn btn-default']])
+            ->add('codigoRequisitoConceptoPk', TextType::class, array('required' => false))
+            ->add('nombre', TextType::class, array('required' => false))
+            ->add('btnFiltrar', SubmitType::class, array('label' => 'Filtrar'))
+            ->add('btnExcel', SubmitType::class, array('label' => 'Excel'))
+            ->add('btnEliminar', SubmitType::class, array('label' => 'Eliminar'))
+            ->add('limiteRegistros', TextType::class, array('required' => false, 'data' => 100))
+            ->setMethod('GET')
             ->getForm();
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        $raw = [
+            'limiteRegistros' => $form->get('limiteRegistros')->getData()
+        ];
+        if ($form->isSubmitted()) {
+            if ($form->get('btnFiltrar')->isClicked()) {
+                $raw['filtros'] = $this->getFiltros($form);
+            }
             if ($form->get('btnExcel')->isClicked()) {
-                General::get()->setExportar($em->getRepository($this->clase)->parametrosExcel(), "Excel");
+                General::get()->setExportar($em->getRepository(RhuRequisitoConcepto::class)->lista($raw), "Requisitos conceptos");
             }
             if ($form->get('btnEliminar')->isClicked()) {
-                $arData = $request->request->get('ChkSeleccionar');
-                $this->get("UtilidadesModelo")->eliminar(RhuRequisitoConcepto::class, $arData);
+                $arrSeleccionados = $request->query->get('ChkSeleccionar');
+                $em->getRepository(RhuRequisitoConcepto::class)->eliminar($arrSeleccionados);
             }
         }
-        $arRequisitosConceptos = $paginator->paginate($em->getRepository(RhuRequisitoConcepto::class)->lista(), $request->query->getInt('page', 1), 50);
+        $arRequisitosConceptos = $paginator->paginate($em->getRepository(RhuRequisitoConcepto::class)->lista($raw), $request->query->getInt('page', 1), 50);
         return $this->render('recursohumano/administracion/contratacion/requisitoconcepto/lista.html.twig', [
             'arRequisitosConceptos' => $arRequisitosConceptos,
             'form' => $form->createView(),
@@ -75,11 +76,34 @@ class RequisitoConceptoController extends ControllerListenerGeneral
                 $em->persist($arRequisitoConceptos);
                 $em->flush();
             }
-            return $this->redirect($this->generateUrl('recursohumano_administracion_contratacion_requisitoconcepto_lista'));
+            return $this->redirect($this->generateUrl('recursohumano_administracion_contratacion_requisitoconcepto_detalle', array('id' => $arRequisitoConceptos->getCodigoRequisitoConceptoPk())));
         }
         return $this->render('recursohumano/administracion/contratacion/requisitoconcepto/nuevo.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    /**
+     * @Route("/recursohumano/administracion/contratacion/requisitoconcepto/detalle/{id}", name="recursohumano_administracion_contratacion_requisitoconcepto_detalle")
+     */
+    public function detalle(Request $request, PaginatorInterface $paginator,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $arRequisitosConcepto = $em->getRepository(RhuRequisitoConcepto::class)->find($id);
+        return $this->render('recursohumano/administracion/contratacion/requisitoconcepto/detalle.html.twig', [
+            'arRequisitosConcepto' => $arRequisitosConcepto,
+        ]);
+    }
+
+    public function getFiltros($form)
+    {
+        $filtro = [
+            'codigoRequisitoConcepto' => $form->get('codigoRequisitoConceptoPk')->getData(),
+            'nombre' => $form->get('nombre')->getData(),
+        ];
+
+        return $filtro;
+
     }
 }
 

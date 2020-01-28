03e07d492db47733f56a9e50cd0160cc39460db4
kml1997 <kevin.moncada.903@unisabaneta.edu.co>
2020-01-28 09:50:34 -0500

    Feat: Se crean controladores faltantes. Fix: Arreglo de GenModelo en DataFixtures.

--- src/Controller/Documental/Administracion/Documental/DocArchivoController.php
+++ src/Controller/Documental/Administracion/Documental/DocArchivoController.php
@@ -1,30 +1,18 @@
 <?php
 
-namespace App\Controller\Administracion\Documental;
+namespace App\Controller\Documental\Administracion\Documental;
 
 
 use App\Controller\MaestroController;
 use App\Entity\Documental\DocArchivo;
 use App\Entity\Documental\DocConfiguracion;
-use App\Entity\Documental\DocDirectorio;
-use App\Entity\Documental\DocMasivo;
-use App\Entity\Documental\DocMasivoCarga;
-use App\Entity\Documental\DocMasivoTipo;
-use App\Entity\Financiero\FinRegistroInconsistencia;
-use App\Entity\Transporte\TteGuia;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\HttpFoundation\Response;
+use App\Form\Type\Documental\ConfiguracionType;
+use App\Utilidades\Mensajes;
+use PhpParser\Comment\Doc;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use App\Utilidades\Mensajes;
+
 class DocArchivoController extends MaestroController
 {
 
@@ -32,13 +20,11 @@ class DocArchivoController extends MaestroController
     public $modelo = "DocArchivo";
 
     protected $clase = DocArchivo::class;
-    protected $claseNombre = "Administraion";
-    protected $modulo = "General";
+    protected $claseNombre = "DocArchivo";
+    protected $modulo = "Documental";
     protected $funcion = "Administracion";
     protected $grupo = "Documental";
-    protected $nombre = "Documental";
-
+    protected $nombre = "DocArchivo";
 
 
 }
-
